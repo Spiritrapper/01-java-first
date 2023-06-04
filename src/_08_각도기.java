a4b2f95e06cadbdd023383aa01bd0b41b43f8c82
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 16:41:29 +0900

    주요 수정 사항:
    코드 간단하게

--- src/_08_각도기.java
+++ src/_08_각도기.java
@@ -9,23 +9,23 @@ class Solution7 {
         } else if (angle == 180) {
             return 4;
         } else {
-            return 0; // 각도 범위에 맞지 않을 경우 0을 반환하도록 설정
+            return 0;
         }
     }
 
     public static void main(String[] args) {
         Solution7 s = new Solution7();
-        double angle = 70;
-        int result1 = s.solution(angle);
-        System.out.println(result1);
-        angle = 91;
-        int result2 = s.solution(angle);
-        System.out.println(result2);
-        angle = 180;
-        int result3 = s.solution(angle);
-        System.out.println(result3);
+        System.out.println(s.solution(70));
+        System.out.println(s.solution(91));
+        System.out.println(s.solution(180));
     }
 }
+
+
+
+
+
+
 
 
 public class _08_각도기 {
