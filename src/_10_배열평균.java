60519884492337f57f359f3f40a74f34b62c8a60
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 18:32:21 +0900

    중간과정 numv=bers.length double average = sum / numbers.length

--- src/_10_배열평균.java
+++ src/_10_배열평균.java
@@ -1,18 +1,21 @@
 class Solution9 {
-    public double solution(int[] numbers){
-        int[3] =[1,2,3];
-
+    public double solution(int[] numbers) {
         int sum = 0;
-        for (int i = 0; i < arrange.lenth; i++) {
-            int average = sum / i;
-            sum += i;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
         }
+        double average = (double) sum / numbers.length;
         return average;
-    } public static void main(String[] args){
-        Solution s = new Solution();
+    }
 
-        System.out.println();
-
+    public static void main(String[] args) {
+        Solution9 s = new Solution9();
+        int[] numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        double result1 = s.solution(numbers1);
+        System.out.println(int[] numbers1 + result1);
+        int[] numbers2 =[89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99];
+        double result2 = s.solution(numbers2);
+        System.out.println(int[] numbers2 + result2);
     }
 }
 
