d9e3f358c5786a3045ef6f735a6bb829d8f9ca66
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 12:51:59 +0900

    위의 코드에서 solution 함수는 num1에서 num2를 뺀 값을 반환합니다. main 함수에서는 예시 입력에 따라 solution 함수를 호출하고, 결과 값을 출력합니다.
    해당 코드를 실행하면 예시 입력에 따라 -1과 98이 출력됩니다.

--- src/_02_두수의뺀값리턴.java
+++ src/_02_두수의뺀값리턴.java
@@ -1,5 +1,4 @@
-
-public class  _02_두수의뺀값리턴 {
+public class _02_두수의뺀값리턴 {
     public static int solution(int num1, int num2) {
         int result = num1 - num2;
         return result;
@@ -8,8 +7,12 @@ public class  _02_두수의뺀값리턴 {
     public static void main(String[] args) {
         int num1 = 2;
         int num2 = 3;
-        int result = solution(num1, num2);
-        System.out.println(result); // 출력: -1
+        int result1 = solution(num1, num2);
+        System.out.println(result1); // 출력: -1
+
+        int num3 = 100;
+        int num4 = 2;
+        int result2 = solution(num3, num4);
+        System.out.println(result2); // 출력: 98
     }
 }
-
