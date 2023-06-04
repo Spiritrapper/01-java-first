60839042bff7413e42a10425b92445ad390e3b23
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 17:04:41 +0900

    수정된 사안
    변수명 충돌을 해결하기 위해 for 루프의 변수를 i로 변경하고, sum 변수를 올바른 위치로 이동하여 루프에서 더하는 동작을 수행하도록 하였습니다

--- src/_09_짝수의합.java
+++ src/_09_짝수의합.java
@@ -1,23 +1,22 @@
 class Solution8 {
-    public int solution(int n){
+    public int solution(int n) {
         int sum = 0;
-        for (int n = 0; n < 1000; n++) {
-            if(n % 2==0){
-                return n;
-            }else
-                return 0;
-            sum += n;
-
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
         }
+        return sum;
     }
-    public static void main (String[] args){
-        Solution S = new Solution();
-        System.out.println(SUM);
 
-
+    public static void main(String[] args) {
+        Solution8 s = new Solution8();
+        int result1 = s.solution(10);
+        System.out.println(result1);
+        int result2 = s.solution(4);
+        System.out.println(result2);
     }
 }
-
 
 
 
