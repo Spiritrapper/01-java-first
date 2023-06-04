e20635b0aeb4ddb5d95d6c4fcea8995a7a171570
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 14:35:16 +0900

    문제가 많네

---
+++ src/_05_숫자비교하기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        if (num1 = num2){
+            return 1;
+        }else
+            return -1;
+
+
+    }
+    public static void main (Stirng[] args){
+        Solution s = new Solution();
+        int resut1 = s.solution(2,3)
+        System.out.print(resut1)
+        int resut1 = s.solution(11,11)
+        System.out.print(resut2)
+        int resut1 = s.solution(7,99)
+        System.out.print(resut3)
+    }
+}
+public class _05_숫자비교하기 {
+}
