4ba907fb564b4a12f047008d96b7d4356e0edc63
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 12:33:59 +0900

    java: variable result is already defined in method solution(int,int)

---
+++ src/_02_두수의뺀값리턴.java
@@ -0,0 +1,17 @@
+class Main {
+    public static int solution(int num1, int num2) {
+        int result = solution(num1, num2);
+        int result = num1 - num2;
+        return result;
+    }
+}
+public class _02_두수의뺀값리턴 {
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+        System.out.println((num1 - num2) + result); // 출력: -1
+
+    }
+}
+
+
