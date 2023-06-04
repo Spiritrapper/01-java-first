369870452f3cdbc64919e3bbd40060fef0c62d0a
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 17:01:04 +0900

    문제는 다음과 같습니다:
    변수명 충돌: solution 메서드의 매개변수 n과 for 루프 내부의 변수 n이 충돌하고 있습니다. 이로 인해 for 루프에서 n을 사용할 수 없게 됩니다.
    
    잘못된 반환 위치: for 루프 내부에서 조건문을 통해 n이 짝수인 경우 n을 반환하고 있습니다. 이로 인해 첫 번째 반복에서 n이 0이기 때문에 0이 반환되고, 메서드가 종료되어 루프가 실행되지 않습니다.
    
    sum 변수의 사용: sum 변수는 현재 루프의 return 문 이후에 있기 때문에, 실제로는 루프에서 더하는 동작을 수행하지 못하고 있습니다.

---
+++ src/_09_짝수의합.java
@@ -0,0 +1,27 @@
+class Solution8 {
+    public int solution(int n){
+        int sum = 0;
+        for (int n = 0; n < 1000; n++) {
+            if(n % 2==0){
+                return n;
+            }else
+                return 0;
+            sum += n;
+
+        }
+    }
+    public static void main (String[] args){
+        Solution S = new Solution();
+        System.out.println(SUM);
+
+
+    }
+}
+
+
+
+
+
+
+public class _09_짝수의합 {
+}
