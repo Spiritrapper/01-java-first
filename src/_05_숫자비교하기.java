aeb2d1336ea976066f206417095e8ba3e37014d6
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 14:37:04 +0900

    주요 수정 사항: class 중복  Solution4 조건문에서 = 대신 ==를 사용하여 동등성을 비교해야 합니다. System.out.print 대신 System.out.println을 사용하여 줄 바꿈이 추가되도록 합니다. 변수 이름에 오타가 있었는데, result1, result2, result3로 수정하였습니다.

--- src/_05_숫자비교하기.java
+++ src/_05_숫자비교하기.java
@@ -1,20 +1,20 @@
-class Solution {
+class Solution4 {
     public int solution(int num1, int num2) {
-        if (num1 = num2){
+        if (num1 == num2) {
             return 1;
-        }else
+        } else {
             return -1;
-
-
+        }
     }
-    public static void main (Stirng[] args){
-        Solution s = new Solution();
-        int resut1 = s.solution(2,3)
-        System.out.print(resut1)
-        int resut1 = s.solution(11,11)
-        System.out.print(resut2)
-        int resut1 = s.solution(7,99)
-        System.out.print(resut3)
+
+    public static void main(String[] args) {
+        Solution4 s = new Solution4();
+        int result1 = s.solution(2, 3);
+        System.out.println(result1);
+        int result2 = s.solution(11, 11);
+        System.out.println(result2);
+        int result3 = s.solution(7, 99);
+        System.out.println(result3);
     }
 }
 public class _05_숫자비교하기 {
