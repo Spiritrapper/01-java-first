be8b3b78044a6ba58e22be9760008aeee73d54a7
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 14:23:38 +0900

    소문자 고침 class Solution의 새로운객체 Solution s = new solution();

---
+++ src/_05_나머지구하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int result = num1 % num2;
+
+        return num1 % num2;
+    }
+    public static void main(String[] args){
+        Solution s = new Solution();
+        int result1 = s.solution(3,2);
+        System.out.print(result1);
+        int result2 =s.solution(10,5);
+        System.out.print(result2);
+
+    }
+}
+public class _05_나머지구하기 {
+}
