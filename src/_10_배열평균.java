861d0a62f8fdd65f3e463eb6160edb78bf1bf5c9
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 18:48:57 +0900

    주어진 배열 numbers의 원소들을 모두 더한 후 배열의 길이로 나누어 평균값을 계산하고 반환합니다. 이를 위해 sum 변수를 사용하여 누적 합을 계산하고, numbers.length를 사용하여 배열의 길이를 구합니다. 마지막으로 (double) 형변환을 통해 정수 나눗셈이 아닌 실수 나눗셈으로 평균값을 계산하게 됩니다.
    main 메서드에서는 주어진 입력 예시에 대해 solution 함수를 호출하여 결과를 출력하는 예시 코드를 작성하였습니다. numbers1과 numbers2는 주어진 입력 예시에 대응되는 배열을 나타내며, 각각의 평균값이 출력됩니다.

--- src/_10_배열평균.java
+++ src/_10_배열평균.java
@@ -10,7 +10,7 @@ class Solution {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] numbers1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] numbers1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         double result1 = s.solution(numbers1);
         System.out.println(result1);
 
