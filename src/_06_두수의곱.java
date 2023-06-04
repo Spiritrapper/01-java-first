d09f2f0e4f38b8349198472f23545457550e628e
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 15:46:21 +0900

    주요 수정 사항:
    result1과 result2 변수를 선언해주어야 합니다.
    System.out.print 대신 System.out.println을 사용하여 줄 바꿈이 추가되도록 합니다.

--- src/_06_두수의곱.java
+++ src/_06_두수의곱.java
@@ -1,17 +1,16 @@
 class Solution5 {
     public int solution(int num1, int num2) {
-        int result =num1 *  num2;
-
-        return num1 *  num2;
+        int result = num1 * num2;
+        return result;
     }
-    public static void main (String[] args) {
+
+    public static void main(String[] args) {
         Solution5 s = new Solution5();
-        result1=s.solution(3,4);
-        System.out.print(result1);
-        result2=s.solution(27,19);
-        System.out.print(result2);
+        int result1 = s.solution(3, 4);
+        System.out.println(result1);
+        int result2 = s.solution(27, 19);
+        System.out.println(result2);
     }
 }
-
 public class _06_두수의곱 {
 }
