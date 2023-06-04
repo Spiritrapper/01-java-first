6c2de2bcda906b6a68bf84578ac2a69edbeb0376
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 13:13:54 +0900

    주어진 코드에는 몇 가지 문제가 있습니다:
    solution 메서드 호출 시 메서드를 호출하는 방식이 잘못되었습니다. 정적 메서드인 solution을 호출하기 위해서는 Solution.solution(num1, num2)와 같이 클래스 이름을 통해 호출해야 합니다.
    
    main 메서드에서 result 변수를 중복하여 선언하고 있습니다. result 변수는 한 번만 선언되어야 합니다.
    
    System.out.print 대신 System.out.println을 사용해야 합니다.
    
    메서드 호출 시 매개변수에 타입을 명시할 필요가 없습니다. solution(num1, num2)와 같이 타입을 생략하여 호출해야 합니다.

---
+++ src/_03_두수의합.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int result = num1 + num2;
+
+        return result;
+    }
+
+    public static void main (String[] args){
+
+        int num1 =2;
+        int num2 =3;
+        int result = solution(int num1, int num2)
+        System.out.print(result);
+        int num1 =100;
+        int num2 =2;
+        int result = solution(int num1, int num2)
+        Systen.out.print(result);
+    }
+}
+public class _03_두수의합 {
+}
