25eb6714e44d58c51dfcf5b5ece4b45842608da5
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 14:02:11 +0900

    code make a shorter

---
+++ src/_04_몫구하기.java
@@ -0,0 +1,19 @@
+class Solution3 {
+    public int solution(int num1, int num2) {
+        int result = num1 /  num2;
+
+        return num1 /  num2 ;
+    }
+    public static void main(String[] args){
+        Solution3 s = new Solution3 ();
+        int result1=s.solution(10,5);
+        System.out.print(result1);
+        int result2=s.solution(7,2);
+        System.out.print(result2);
+    }
+}
+
+
+
+public class _04_몫구하기 {
+}
