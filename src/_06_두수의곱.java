7acfed19f083726c4ba88433c6d4ad9250be53c5
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 15:45:13 +0900

    java: cannot find symbol   symbol:   variable result1   location: class Solution5

---
+++ src/_06_두수의곱.java
@@ -0,0 +1,17 @@
+class Solution5 {
+    public int solution(int num1, int num2) {
+        int result =num1 *  num2;
+
+        return num1 *  num2;
+    }
+    public static void main (String[] args) {
+        Solution5 s = new Solution5();
+        result1=s.solution(3,4);
+        System.out.print(result1);
+        result2=s.solution(27,19);
+        System.out.print(result2);
+    }
+}
+
+public class _06_두수의곱 {
+}
