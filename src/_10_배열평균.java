5ebd672926c05567690348b179ebc4fcd5c552fd
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 18:21:28 +0900

    주어진 코드에서 int[3] =[1,2,3]; 부분은 문법적으로 올바르지 않습니다. 배열 선언 및 초기화는 int[] numbers = {1, 2, 3}; 형태로 작성해야 합니다. 또한, average 변수는 반복문 안에서 계산되어야 하며, sum += i; 문장이 sum += numbers[i];로 수정되어야 합니다

---
+++ src/_10_배열평균.java
@@ -0,0 +1,24 @@
+class Solution9 {
+    public double solution(int[] numbers){
+        int[3] =[1,2,3];
+
+        int sum = 0;
+        for (int i = 0; i < arrange.lenth; i++) {
+            int average = sum / i;
+            sum += i;
+        }
+        return average;
+    } public static void main(String[] args){
+        Solution s = new Solution();
+
+        System.out.println();
+
+    }
+}
+
+
+
+
+
+public class _10_배열평균 {
+}
