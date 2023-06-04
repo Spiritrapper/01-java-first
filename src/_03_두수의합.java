e345c7793d40ac835da87cd251772b1a5f146c38
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 13:25:12 +0900

    1.java: duplicate class: Solution 수정 Solution2 Solution2 solution = new Solution2();

--- src/_03_두수의합.java
+++ src/_03_두수의합.java
@@ -1,21 +1,23 @@
-class Solution {
+class Solution2 {
     public int solution(int num1, int num2) {
         int result = num1 + num2;
-
         return result;
     }
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
+        Solution2 solution = new Solution2();
 
-        int num1 =2;
-        int num2 =3;
-        int result = solution(int num1, int num2)
-        System.out.print(result);
-        int num1 =100;
-        int num2 =2;
-        int result = solution(int num1, int num2)
-        Systen.out.print(result);
+        int num1 = 2;
+        int num2 = 3;
+        int result1 = solution.solution(num1, num2);
+        System.out.println(result1);
+
+        num1 = 100;
+        num2 = 2;
+        int result2 = solution.solution(num1, num2);
+        System.out.println(result2);
     }
 }
+
 public class _03_두수의합 {
 }
