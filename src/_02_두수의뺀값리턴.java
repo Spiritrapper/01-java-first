737aa86f762678f48882881d7686ab54e20c936b
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 12:39:35 +0900

    수정된 코드에서는 result 변수를 solution 함수 내부에서 선언하고, num1 - num2 값을 result에 할당한 후에 반환합니다. 그리고 main 함수에서는 solution 함수를 호출하여 결과 값을 result 변수에 저장하고, System.out.println(result);를 사용하여 결과를 출력합니다.
    위의 코드를 실행하면 -1이 출력됩니다.
    
    따라서, 수정된 코드는 주어진 조건에 맞게 num1에서 num2를 뺀 값을 반환하고 출력하는 예시입니다.

--- src/_02_두수의뺀값리턴.java
+++ src/_02_두수의뺀값리턴.java
@@ -1,17 +1,15 @@
-class Main {
+
+public class  _02_두수의뺀값리턴 {
     public static int solution(int num1, int num2) {
-        int result = solution(num1, num2);
         int result = num1 - num2;
         return result;
     }
-}
-public class _02_두수의뺀값리턴 {
+
     public static void main(String[] args) {
         int num1 = 2;
         int num2 = 3;
-        System.out.println((num1 - num2) + result); // 출력: -1
-
+        int result = solution(num1, num2);
+        System.out.println(result); // 출력: -1
     }
 }
-
 
