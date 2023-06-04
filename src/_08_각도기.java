737815e0e17428a796cfca8112aa3360c511d9fa
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 16:37:42 +0900

    주요 수정 사항:
    switch 문이 아닌 if-else if 문을 사용하여 각도 범위를 체크하도록 수정하였습니다.
    switch 문의 조건 부분(case)에는 상수만 사용할 수 있기 때문에 부등호를 사용할 수 없습니다. 따라서 if-else if 문으로 변경하였습니다.
    각도 범위에 맞지 않는 경우를 처리하기 위해 마지막에 else 블록을 추가하여 -1을 반환하도록 설정하였습니다.

--- src/_08_각도기.java
+++ src/_08_각도기.java
@@ -1,21 +1,30 @@
-class Solution7{
-    public int solution(int angle) {
-        switch (angle) {
-            case 0< angle <90 :
-                return 1;
-            case angle = 90:
-                return 2;
-            case 90 < angle < 180:
-                return 3;
-            case angle =180:
-                return 4;
-    }
-    public static void main (String[] args){
-            Solution7 s = new Solution7();
-            double angle = 95.5;
-            int result = s.solution(angle);
-            System.out.println(result);
+class Solution7 {
+    public int solution(double angle) {
+        if (angle > 0 && angle < 90) {
+            return 1;
+        } else if (angle == 90) {
+            return 2;
+        } else if (angle > 90 && angle < 180) {
+            return 3;
+        } else if (angle == 180) {
+            return 4;
+        } else {
+            return 0; // 각도 범위에 맞지 않을 경우 0을 반환하도록 설정
         }
+    }
+
+    public static void main(String[] args) {
+        Solution7 s = new Solution7();
+        double angle = 70;
+        int result1 = s.solution(angle);
+        System.out.println(result1);
+        angle = 91;
+        int result2 = s.solution(angle);
+        System.out.println(result2);
+        angle = 180;
+        int result3 = s.solution(angle);
+        System.out.println(result3);
+    }
 }
 
 
