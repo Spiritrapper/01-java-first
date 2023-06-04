d030a084ff15561b5135a4b84c3a8e96027f0a46
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 13:43:46 +0900

    code make a shorter

--- src/_03_두수의합.java
+++ src/_03_두수의합.java
@@ -1,20 +1,20 @@
 class Solution2 {
     public int solution(int num1, int num2) {
-        int result = num1 + num2;
-        return result;
+       // int result = num1 + num2;
+        return num1 + num2;
     }
 
     public static void main(String[] args) {
         Solution2 solution = new Solution2();
-
-        int num1 = 2;
-        int num2 = 3;
-        int result1 = solution.solution(num1, num2);
+//
+//        int num1 = 2;
+//        int num2 = 3;
+        int result1 = solution.solution(2, 3);
         System.out.println(result1);
-
-        num1 = 100;
-        num2 = 2;
-        int result2 = solution.solution(num1, num2);
+//
+//        num1 = 100;
+//        num2 = 2;
+        int result2 = solution.solution(100, 2);
         System.out.println(result2);
     }
 }
