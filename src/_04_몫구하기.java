366b1d55c34102023b0a9505a8b23609d1c448e3
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-04 14:03:41 +0900

    몫 / 나머지 %

--- src/_04_몫구하기.java
+++ src/_04_몫구하기.java
@@ -1,7 +1,6 @@
 class Solution3 {
     public int solution(int num1, int num2) {
         int result = num1 /  num2;
-
         return num1 /  num2 ;
     }
     public static void main(String[] args){
