d6f2f66cef5d1c65cec649b0fd9c499051df2263
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-05 11:28:50 +0900

    오류 java: non-static variable this cannot be referenced from a static context
    정적(static) 컨텍스트에서 비정적(non-static) 변수인 this를 참조할 수 없다는 오류 메시지입니다.
    
    이 오류는 비정적인 컨텍스트에서 사용되어야 하는 this 키워드가 정적인 컨텍스트에서 사용되려고 할 때 발생합니다. 정적(static) 메서드에서는 클래스의 인스턴스에 대한 참조를 직접적으로 사용할 수 없습니다.
    
    위의 코드에서 main 메서드는 정적인(static) 메서드입니다. 내부 클래스인 Solution12의 인스턴스를 생성하려고 할 때 this 키워드를 사용하면 위와 같은 오류가 발생합니다.
    
    이 문제를 해결하기 위해서는 Solution12 클래스를 정적(static)으로 선언하거나, main 메서드 내에서 Solution12 클래스의 인스턴스를 생성하여 사용해야 합니다.
    
    예를 들어, Solution12 클래스를 정적(static)으로 선언하려면 다음과 같이 코드를 수정할 수 있습니다:

--- src/_12_배열자르기.java
+++ src/_12_배열자르기.java
@@ -1,29 +1,45 @@
 import java.util.Arrays;
-
-class Solution12 {
-    public int[] solution(int[] numbers, int num1, int num2) {
-        return Arrays.copyOfRange(numbers, num1, num2 + 1);
-    }
-
-    public static void main(String[] args) {
-        Solution12 s = new Solution12();
-        int[] numbers1 = {1, 2, 3, 4, 5};
-        int num1 = 1;
-        int num2 = 3;
-        int[] result1 = s.solution(numbers1, num1, num2);
-        System.out.println(Arrays.toString(result1));
-
-        int[] numbers2 = {1, 3, 5};
-        num1 = 1;
-        num2 = 2;
-        int[] result2 = s.solution(numbers2, num1, num2);
-        System.out.println(Arrays.toString(result2));
-    }
-}
+//
+//class Solution12 {
+//    public int[] solution(int[] numbers, int num1, int num2) {
+//        return Arrays.copyOfRange(numbers, num1, num2 + 1);
+//    }
+//
+//    public static void main(String[] args) {
+//        Solution12 s = new Solution12();
+//        int[] numbers1 = {1, 2, 3, 4, 5};
+//        int num1 = 1;
+//        int num2 = 3;
+//        int[] result1 = s.solution(numbers1, num1, num2);
+//        System.out.println(Arrays.toString(result1));
+//
+//        int[] numbers2 = {1, 3, 5};
+//        num1 = 1;
+//        num2 = 2;
+//        int[] result2 = s.solution(numbers2, num1, num2);
+//        System.out.println(Arrays.toString(result2));
+//    }
+//}
 
 
 
 
 
 public class _12_배열자르기 {
+
+    class Solution12 {
+        public int[] solution(int[] numbers, int num1, int num2) {
+            return Arrays.copyOfRange(numbers, num1, num2 + 1);
+        }
+
+        public static void main(String[] args) {
+            Solution12 s = new Solution12();
+            int[] numbers1 = {1, 2, 3, 4, 5};
+            System.out.print(s.solution(numbers1, 1, 3));
+            int[] numbers2 = {1, 3, 5, 7, 9};
+            System.out.print(s.solution(numbers2, 1, 3));
+
+        }
+
+    }
 }
