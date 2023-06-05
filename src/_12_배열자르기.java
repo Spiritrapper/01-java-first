03e0a2f55a9ae79706f3008da44d97d77556d13f
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-05 11:11:58 +0900

    다른 접근 방식 중 하나는 슬라이싱(Slicing)을 활용하는 것입니다. 슬라이싱은 배열이나 리스트에서 원하는 범위의 요소를 추출하는 방법입니다. Java에서는 배열을 슬라이싱하기 위해 Arrays.copyOfRange() 메소드를 사용할 수 있습니다.
    num2 + 1이 사용되는 이유는, Arrays.copyOfRange 메소드에서 범위의 끝 인덱스는 복사 대상에 포함되지 않기 때문입니다. 즉, num2까지만 복사하면 num2 인덱스의 요소는 포함되지 않게 됩니다.

--- src/_12_배열자르기.java
+++ src/_12_배열자르기.java
@@ -2,11 +2,7 @@ import java.util.Arrays;
 
 class Solution12 {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int[] answer = new int[num2 - num1 + 1];   // 문제 answer 배열선언 특정 배열의 크기는 int[end - start+1] 여기서 end start 전부 인덱스값 = 위치값
-        for (int i = num1; i <= num2; i++) {
-            answer[i - num1] = numbers[i];         // 특정배열위치인덱스값은 answer[i-start] 0부터 시작하기때문에
-        }
-        return answer;
+        return Arrays.copyOfRange(numbers, num1, num2 + 1);
     }
 
     public static void main(String[] args) {
