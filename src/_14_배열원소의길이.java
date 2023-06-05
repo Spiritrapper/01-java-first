d095d016cdf2b135360a8ba8b3ad14b5301d1403
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-05 13:14:46 +0900

    수정 사안
    먼저, int[] answer 배열을 선언하여 결과를 저장할 배열을 생성합니다.
    for 루프를 사용하여 strlist의 각 원소의 길이를 구하여 answer 배열에 저장합니다. 이때, strlist[i].length()를 사용하여 각 원소의 길이를 구합니다.
    solution 메소드가 int[] 타입의 결과 배열을 반환하도록 변경합니다.
    main 메소드에서 solution 메소드를 호출하여 결과를 얻고, 이를 출력합니다. 각 원소의 길이를 출력하기 위해 for-each 루프를 사용하였습니다.
    이렇게 수정된 코드는 strlist 배열의 각 원소의 길이를 구하여 결과 배열로 반환하는 기능을 수행합니다.

--- src/_14_배열원소의길이.java
+++ src/_14_배열원소의길이.java
@@ -1,14 +1,25 @@
-class Solution {
+class Solution14 {
     public int[] solution(String[] strlist) {
-        String[] strlist = {str.length(),}
-        int[] answer = {};
-        return int strlist1[] ;
+        int[] answer = new int[strlist.length]; // 결과를 저장할 배열 생성
+
+        // 각 원소의 길이를 구하여 answer 배열에 저장
+        for (int i = 0; i < strlist.length; i++) {
+            answer[i] = strlist[i].length();
+        }
+
+        return answer; // 결과 배열 반환
     }
 
     public static void main(String[] args) {
-        Solution s = new Solution();
-        String [] strlist1 = {"We", "are", "the", "world!"};
+        Solution14 s = new Solution14();
+        String[] strlist1 = {"We", "are", "the", "world!"};
 
+        int[] result = s.solution(strlist1);
+
+        // 결과 출력
+        for (int length : result) {
+            System.out.print(length + " ");
+        }
     }
 }
 
