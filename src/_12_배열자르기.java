611dc4f3e22455edaf92e9cc8b81c1888831e113
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-05 10:56:16 +0900

    아래와 같이 import java.util.Arrays; 문장을 코드 상단에 추가 이 줄은 Solution12라는 이름의 클래스를 선언합니다 이 줄은 solution이라는 이름의 메서드를 정의합니다. 이 메서드는 정수 배열 numbers와 두 개의 정수 num1과 num2를 매개변수로 받습니다. 정수 배열을 반환합니다. 이 줄은 answer라는 이름의 정수 배열을 선언합니다. 배열의 크기는 num2 - num1 + 1입니다. 이는 numbers 배열에서 num1부터 num2까지의 원소를 저장할 배열의 크기를 나타냅니다.
    이 줄은 for 반복문을 사용하여 num1부터 num2까지의 인덱스를 반복합니다. 반복문 내에서 answer[i - num1]에 numbers[i]의 값을 저장합니다. 이를 통해 numbers 배열에서 해당 범위의 값을 answer 배열로 복사합니다.
    이 줄은 answer 배열을 반환합니다. 이 배열은 numbers 배열에서 num1부터 num2까지의 값을 가지고 있습니다.
    이 부분은 main 메서드입니다. 프로그램의 진입점입니다. 이 코드에서는 Solution12 클래스의 인스턴스를 생성하고, 주어진 예시 입력값을 사용하여 solution 메서드를 호출한 후 반환된 결과를 출력합니다.

--- src/_12_배열자르기.java
+++ src/_12_배열자르기.java
@@ -1,15 +1,27 @@
+import java.util.Arrays;
+
 class Solution12 {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int[] number = {1, 2, ..., 30};
-
+        int[] answer = new int[num2 - num1 + 1];   // 문제 answer 배열선언 특정 배열의 크기는 int[end - start+1] 여기서 end start 전부 인덱스값 = 위치값
+        for (int i = num1; i <= num2; i++) {
+            answer[i - num1] = numbers[i];         // 특정배열위치인덱스값은 answer[i-start] 0부터 시작하기때문에
+        }
         return answer;
     }
 
     public static void main(String[] args) {
+        Solution12 s = new Solution12();
+        int[] numbers1 = {1, 2, 3, 4, 5};
+        int num1 = 1;
+        int num2 = 3;
+        int[] result1 = s.solution(numbers1, num1, num2);
+        System.out.println(Arrays.toString(result1));
 
-
-        System.out.println(solution[0][1]);
-
+        int[] numbers2 = {1, 3, 5};
+        num1 = 1;
+        num2 = 2;
+        int[] result2 = s.solution(numbers2, num1, num2);
+        System.out.println(Arrays.toString(result2));
     }
 }
 
