26a84bdd6fdc5d1c028b0d7da7efe72283d05ce5
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-05 11:39:10 +0900

    수정 사안
    java: Illegal static declaration in inner class _12_배열자르기.Solution12 오류는 내부 클래스인 Solution12에서 static으로 선언된 메서드 또는 필드가 존재할 경우 발생합니다.
    
    위의 코드에서 Solution12 클래스는 내부 클래스로 선언되어 있고, main 메서드가 내부 클래스 안에 위치하고 있습니다. 내부 클래스의 특징은 외부 클래스의 인스턴스에 종속적이며, 정적(static) 멤버를 직접 선언할 수 없습니다.
    
    따라서, Solution12 클래스 안에 main 메서드를 정의할 때 static 키워드를 제거해야 합니다. 수정된 코드는 다음과 같습니다:

--- src/_12_배열자르기.java
+++ src/_12_배열자르기.java
@@ -31,16 +31,15 @@ public class _12_배열자르기 {
         public int[] solution(int[] numbers, int num1, int num2) {
             return Arrays.copyOfRange(numbers, num1, num2 + 1);
         }
+    }
 
-        public static void main(String[] args) {
-            _12_배열자르기 outer = new _12_배열자르기(); // 외부 클래스의 인스턴스 생성
-            Solution12 s = outer.new Solution12(); // 내부 클래스의 인스턴스 생성
-            int[] numbers1 = {1, 2, 3, 4, 5};
-            System.out.print(s.solution(numbers1, 1, 3));
-            int[] numbers2 = {1, 3, 5, 7, 9};
-            System.out.print(s.solution(numbers2, 1, 3));
-
-        }
+    public static void main(String[] args) {
+        _12_배열자르기 outer = new _12_배열자르기(); // 외부 클래스의 인스턴스 생성
+        Solution12 s = outer.new Solution12(); // 내부 클래스의 인스턴스 생성
+        int[] numbers1 = {1, 2, 3, 4, 5};
+        System.out.print(Arrays.toString(s.solution(numbers1, 1, 3)));
+        int[] numbers2 = {1, 3, 5, 7, 9};
+        System.out.print(Arrays.toString(s.solution(numbers2, 1, 3)));
 
     }
 }
