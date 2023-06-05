d3d144f20c5b8244e2510d30a7823722d24d7598
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-05 13:11:42 +0900

    solution 메소드 내에 중복된 변수 선언이 존재합니다. strlist라는 배열 변수가 두 번 선언되어 있습니다. 이는 문법적으로 오류가 발생합니다. 변수의 이름을 다르게 지정해야 합니다. int[] answer = {};에서 answer 배열이 비어있는 상태로 초기화되어 있습니다. 이는 정확한 결과를 담을 수 없으므로 수정이 필요합니다. return int strlist1[] ;은 문법적으로 잘못된 구문입니다. 반환할 배열을 answer 배열로 지정해야 합니다. main 메소드에서 solution 메소드를 호출하고 결과를 활용하는 코드가 없습니다. 따라서 결과를 확인하거나 활용할 수 없습니다.
    예를 들어, String message = "Hello";라는 코드가 있다면, message 변수는 문자열 "Hello"를 참조하고 있으므로 message.length()는 5를 반환합니다.
    
    따라서, message.length()는 message 변수가 참조하는 문자열의 길이를 반환하는 것입니다. message 변수는 String 타입으로 선언되어야 하며, String 클래스의 메소드인 length()를 호출하여 문자열의 길이를 확인할 수 있습니다.

---
+++ src/_14_배열원소의길이.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(String[] strlist) {
+        String[] strlist = {str.length(),}
+        int[] answer = {};
+        return int strlist1[] ;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String [] strlist1 = {"We", "are", "the", "world!"};
+
+    }
+}
+
+
+public class _14_배열원소의길이 {
+}
