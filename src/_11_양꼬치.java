85a4cf900f9e5d1b35e027dcf5c9b150c65f204f
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-05 09:15:34 +0900

    주요 수정 사항:
    if(int n < 10) 부분에서 int를 제거하여 if (n < 10)으로 수정합니다.
    for 루프에서 return 문이 반복문 내부에 위치하고 있어, 첫 번째 반복에서만 결과를 반환하고 반복문이 종료됩니다. 원하는 동작을 위해 반복문을 수정합니다.
    모든 조건에 해당하지 않을 경우 0을 반환하기 위해 return 0 문을 추가합니다.
    main 메서드에서 result1을 출력하도록 수정합니다.

--- src/_11_양꼬치.java
+++ src/_11_양꼬치.java
@@ -2,35 +2,34 @@
 //        #include <stdbool.h>
 //        #include <stdlib.h>
 
-class Solution {
+class Solution11 {
     public int solution(int n, int k) {
-        if(int n < 10) {
-        for (int i = 0; i < n ; i++) {
-            int result = 12000 * i + ;
-            return 12000 * i ;
-        }
-        else {
-                for (int i = 0; i < n; i++) {
-                    int result = 12000 * i -i/n * 2000;
-                    return 12000 * i -i/n * 2000;
-                }
-
-                }
+        if (n < 10) {
+            for (int i = 0; i < n; i++) {
+                int result = 12000 * i;
+                return 12000 * i;
             }
-        for (int j = 0; j < k; j++) {
-            int result = 2000 * j ;
-            return 2000 * j;
-
+        } else {
+            for (int i = 0; i < n; i++) {
+                int result = 12000 * i - i / n * 2000;
+                return 12000 * i - i / n * 2000;
+            }
         }
 
+        for (int j = 0; j < k; j++) {
+            int result = 2000 * j;
+            return 2000 * j;
+        }
+
+        return 0; // 모든 조건에 해당하지 않을 경우 0을 반환
     }
 
     public static void main(String[] args) {
-        Solution  s = new Solution();
+        Solution11 s = new Solution11();
         int n = 10;
         int k = 3;
-        int result1 = s.solutiont(10,3);
-
+        int result1 = s.solution(10, 3);
+        System.out.println(result1);
     }
 }
 
