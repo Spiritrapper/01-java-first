47146f91e7bab719c7c1fd161e2f51a2a196034f
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-05 12:24:28 +0900

    문제 사안
    Stringlength() 메소드가 존재하지 않습니다. 올바른 메소드는 message.length()입니다. 따라서 int massageLength = Stringlength();을 int massageLength = message.length();로 수정해야 합니다.
    
    return 2 * massageLength;은 가로 길이를 반환하고자 하는 의도로 보입니다. 그러나 문제에서 요구하는 것은 "편지를 가로로만 적을 때, 축하 문구 message를 적기 위해 필요한 편지지의 최소 가로 길이"입니다. 따라서, return 2 * massageLength;을 return massageLength * 2 + 2;로 수정해야 합니다. 여기서 2를 더하는 이유는 단어 사이에 공백을 추가하기 위해서입니다.

---
+++ src/_13_편지.java
@@ -0,0 +1,19 @@
+import stringlength;
+class Solution {
+    public int solution(String message) {
+        int massageLength = massageLength();
+        int paperLength = 2 * massageLength;
+        return 2 * massageLength;
+    }
+
+    public static void main(String[] args) {
+        Solution S = new Solution();
+
+    }
+}
+
+
+
+
+public class _13_편지 {
+}
