1a860d627d73d30b0298fa816ac2703382fbb472
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-05 21:11:38 +0900

    수정 사안
    이렇게 수정된 코드는 dot 배열에서 좌표값을 가져와 x와 y를 초기화하고, 해당 좌표가 속하는 사분면을 올바르게 판별합니다. main 메소드에서는 입력 좌표와 결과값을 함께 출력합니다.
    
    " => " 추가

--- src/_18_점의위치구하기.java
+++ src/_18_점의위치구하기.java
@@ -2,32 +2,29 @@ import java.util.Arrays;
 
 class Solution18 {
     public int solution(int[] dot) {
-        int x = 0;
-        int y = 0;
-//        int[] dot = {x,y};
-        if (0 < x && 0 < y){
-            return 1;}
-        else if (0 < x && 0 > y){
-            return 4;}
-        else if (0> x && 0 < y ){
+        int x = dot[0];
+        int y = dot[1];
+
+        if (0 < x && 0 < y) {
+            return 1;
+        } else if (0 < x && 0 > y) {
+            return 4;
+        } else if (0 > x && 0 < y) {
             return 2;
-        }
-        else if (0> x && 0 > y){
-            return  3;
-        }else {
+        } else if (0 > x && 0 > y) {
+            return 3;
+        } else {
             return 0;
         }
-
     }
 
     public static void main(String[] args) {
         Solution18 s = new Solution18();
-        int[] dot1 = {2,4};
-        int result1 =s.solution(dot1);
-        System.out.println(Arrays.toString(dot1) + result1);
+        int[] dot1 = {2, 4};
+        int result1 = s.solution(dot1);
+        System.out.println(Arrays.toString(dot1) + " => " + result1);
     }
 }
-
 
 
 
