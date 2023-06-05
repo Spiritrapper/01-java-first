60a16a06ee3ac796bef3d6d818db968d213ecd11
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-05 14:41:23 +0900

    보충 사안 1. 주석 추가 2.왜 public static void main(String[] args) {   Solution s = new Solution(); 솔루션 객체를 생성하는 이유?
    public static void main(String[] args) 메소드는 Java 프로그램의 시작점(entry point)으로 사용되는 메소드입니다. 해당 메소드 내에서 Solution 클래스의 객체를 생성하는 이유는 다음과 같습니다:
    
    Solution 클래스는 solution 메소드를 포함하고 있습니다. 따라서 Solution 클래스의 객체를 생성해야 해당 메소드를 호출할 수 있습니다.
    solution 메소드는 문자열 배열을 매개변수로 받아서 정수 배열을 반환합니다. 따라서 Solution 클래스의 객체를 생성하여 이 메소드를 호출하여 결과를 얻을 수 있습니다.
    따라서 Solution 클래스의 객체를 생성하여 solution 메소드를 호출하는 것은 solution 메소드를 실행하기 위한 준비 과정입니다.

--- src/_14_배열원소의길이.java
+++ src/_14_배열원소의길이.java
@@ -4,11 +4,11 @@ class Solution14 {
     // 문자열 배열(strlist)을 매개변수로 받아서 int 배열을 반환하는 solution 메소드 정의
     public int[] solution(String[] strlist) {
         // 결과를 저장할 int 배열(answer) 생성. 배열의 길이는 strlist 배열의 길이와 동일하다.
-        int[] answer = new int[strlist.length];
+        int[] answer = new int[strlist.length];  // [] 내에는 배열의 길이 위치정보 1, 2, 3, 이런식으로만 나타낸다.
 
         // 각 원소의 길이를 구하여 answer 배열에 저장
         for (int i = 0; i < strlist.length; i++) {
-            answer[i] = strlist[i].length();
+            answer[i] = strlist[i].length();    // 예를 들으면 strlist[1] 은 3이라는 원소값를갖는다.
         }
 
         // 결과 배열 반환
@@ -35,4 +35,12 @@ class Solution14 {
 
 public class _14_배열원소의길이 {
 }
+class Solution {
+    public int[] solution(String[] strlist){
+        int[] answer = new int[strlist.length];
+        for (int i = 0; i < ; i++) {
 
+        }
+
+    }
+}
