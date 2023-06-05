0630ace04956963cf7ef6b1d46166222b51d2ad7
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-05 10:01:11 +0900

    주어진 문제에 대한 접근 방식을 보면, solution 함수는 numbers 배열에서 num1번째 인덱스부터 num2번째 인덱스까지의 부분 배열을 반환하는 것입니다.
    그러나 주어진 코드의 구현 방식에는 몇 가지 문제가 있습니다.
    
    int[] number = {1, 2, ..., 30}; : number 배열을 1부터 30까지의 숫자로 초기화하려는 것 같지만, 중괄호({}) 안에 실제 값들을 적어주어야 합니다. 예를 들어, int[] number = {1, 2, 3, ..., 30};와 같이 초기화해야 합니다.
    
    return answer; : solution 함수에서 반환할 결과인 answer를 구하는 부분이 구현되어 있지 않습니다. answer 배열을 생성하고 numbers 배열에서 num1부터 num2까지의 요소를 복사하여 answer에 저장한 다음, answer 배열을 반환해야 합니다.
    
    System.out.println(solution[0][1]); : solution 메서드는 Solution12 클래스의 인스턴스 메서드이므로, 클래스 인스턴스를 생성한 후에 메서드를 호출해야 합니다. 예를 들어, Solution12 s = new Solution12();로 인스턴스를 생성한 다음 s.solution(...)과 같이 호출해야 합니다.

---
+++ src/_12_배열자르기.java
@@ -0,0 +1,21 @@
+class Solution12 {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] number = {1, 2, ..., 30};
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println(solution[0][1]);
+
+    }
+}
+
+
+
+
+
+public class _12_배열자르기 {
+}
