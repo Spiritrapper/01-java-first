cf1f0b53336983de0676845b8ea88982911bb7a8
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-05 09:09:43 +0900

    양꼬치와 음료수 합계를 나타내네 조건이 못만들겠는데 앞에 조건문으로 solution 아직도 빨간불

---
+++ src/_11_양꼬치.java
@@ -0,0 +1,42 @@
+//#include <stdio.h>
+//        #include <stdbool.h>
+//        #include <stdlib.h>
+
+class Solution {
+    public int solution(int n, int k) {
+        if(int n < 10) {
+        for (int i = 0; i < n ; i++) {
+            int result = 12000 * i + ;
+            return 12000 * i ;
+        }
+        else {
+                for (int i = 0; i < n; i++) {
+                    int result = 12000 * i -i/n * 2000;
+                    return 12000 * i -i/n * 2000;
+                }
+
+                }
+            }
+        for (int j = 0; j < k; j++) {
+            int result = 2000 * j ;
+            return 2000 * j;
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Solution  s = new Solution();
+        int n = 10;
+        int k = 3;
+        int result1 = s.solutiont(10,3);
+
+    }
+}
+
+
+
+
+
+public class _11_양꼬치 {
+}
