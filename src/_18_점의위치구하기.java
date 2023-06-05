58b395fba234d3ced8c798fe4d28d61415c4ea80
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-05 21:05:18 +0900

    문제 사안
    int x = 0;과 int y = 0;를 통해 x와 y를 0으로 초기화하고 있지만, 이후에 x와 y의 값이 변경되지 않고 고정되어 있습니다. 따라서 항상 사분면 0을 반환하게 됩니다.
    
    int[] dot 매개변수가 있지만 사용되지 않습니다. dot 배열이 주어지지 않고 항상 초기화된 x와 y 값만 사용되기 때문에 입력값을 활용하지 않는 문제입니다.
    
    주석 처리된 int[] dot = {x,y}; 코드는 필요하지 않습니다. 이미 dot 배열이 매개변수로 전달되고 있기 때문에 추가적으로 배열을 선언할 필요가 없습니다.

---
+++ src/_18_점의위치구하기.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+class Solution18 {
+    public int solution(int[] dot) {
+        int x = 0;
+        int y = 0;
+//        int[] dot = {x,y};
+        if (0 < x && 0 < y){
+            return 1;}
+        else if (0 < x && 0 > y){
+            return 4;}
+        else if (0> x && 0 < y ){
+            return 2;
+        }
+        else if (0> x && 0 > y){
+            return  3;
+        }else {
+            return 0;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Solution18 s = new Solution18();
+        int[] dot1 = {2,4};
+        int result1 =s.solution(dot1);
+        System.out.println(Arrays.toString(dot1) + result1);
+    }
+}
+
+
+
+
+
+
+
+
+
+public class _18_점의위치구하기 {
+}
