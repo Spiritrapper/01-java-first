112658e5c89d83f73c548bea4b4f78ecf0bb798d
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-05 17:35:12 +0900

    문제 사안
    int money = 0; 변수값의 초기화
    int money = 0; 코드에서 money 변수를 초기화하는 것은 문제의 의도와는 다릅니다.
    
    문제에서 money는 매개변수로 주어지는 값으로, 머쓱이가 가지고 있는 돈의 양을 나타내는 변수입니다. 따라서 money 변수를 초기화하는 것은 문제의 조건을 무시하고 항상 0으로 설정하는 것과 같습니다.
    
    올바른 코드에서는 int money 매개변수로 주어진 값을 그대로 사용하여 계산하고 결과를 반환합니다. 초기화를 따로 할 필요가 없습니다.
    
    변수의 초기화는 해당 변수를 사용하기 전에 값을 할당하는 작업을 의미합니다. 변수를 초기화하는 이유는 변수가 사용되기 전에 정의된 값을 가지도록 하기 위함입니다.
    
    변수를 초기화해야 하는 경우는 다음과 같습니다:
    
    변수를 선언할 때 초기값을 주고자 할 때: 변수를 선언과 동시에 초기값을 할당하여 변수를 초기화할 수 있습니다. 예를 들어, int x = 0;과 같이 변수 x를 선언하고 동시에 초기값 0을 할당합니다.
    
    이전에 할당된 값을 초기화하고자 할 때: 변수에 이전에 할당된 값을 초기화하고 새로운 값을 할당하려는 경우, 초기값을 할당하여 변수를 초기화합니다. 예를 들어, int x = 0;과 선언된 변수 x에 이전에 할당된 값을 초기화한 후, 새로운 값을 할당할 수 있습니다.
    
    객체를 생성하고 초기화할 때: 객체를 생성한 후에는 해당 객체의 멤버 변수들을 초기화해야 합니다. 이는 객체를 생성하여 사용하기 전에 객체의 상태를 정의하기 위함입니다. 이를 위해 생성자나 초기화 블록을 사용하여 객체의 멤버 변수를 초기화할 수 있습니다.
    
    초기화의 필요성은 변수가 정의되는 문맥과 사용되는 시점에 따라 다를 수 있습니다. 적절한 시기에 변수를 초기화하여 의도한 동작을 수행할 수 있도록 해야합니다.

---
+++ src/_16_아이스아메리카노.java
@@ -0,0 +1,26 @@
+class Solution16 {
+    public int[] solution(int money) {
+        int money = 0;
+        int americanoPrice = 5500;
+        int americanoDrinking = money / americanoPrice;
+        int restMoney = money / americanoPrice;
+        int[] result = {americanoDrinking, restMoney};
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution16 s = new Solution16();
+        int money1 = 15000;
+        int[] result1 = s.solution(money1);
+        System.out.println(money1 + result1[0] + result1[1]);
+
+
+    }
+}
+
+
+
+
+
+public class _16_아이스아메리카노 {
+}
