14c70cbe4f50311b6cbe367e74655ef2aa1f9c82
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-06 17:21:02 +0900

    문제 사안
    로직이 잘못됨 출력1만을 보여줌

--- src/_21_삼각형의완성조건.java
+++ src/_21_삼각형의완성조건.java
@@ -3,16 +3,20 @@ class Solution21 {
         for (int i = 0; i < sides.length; i++) {
             for (int j = 0; j < sides.length; j++) {
                 for (int k = 0; k < sides.length; k++) {
+                    if (sides[i] + sides[j] <= sides[k])  // 삼각형 조건을 만족하지 않는 경우
+                        return 1;
                 }
-                if(sides[i] + sides[j] <= sides[k])
-                    return 1;
-
-
             }
+        }
 
-        }else
-            return 3;
+        return 3;  // 모든 경우에 삼각형 조건을 만족하는 경우
+    }
 
+    public static void main(String[] args) {
+        Solution21 s = new Solution21();
+        int[] sides1 = {2, 8, 15};
+        int result1 = s.solution(sides1);
+        System.out.println(result1);
     }
 }
 
