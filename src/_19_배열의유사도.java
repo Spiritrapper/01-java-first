6cc21bbddb388d524216eadb45707f57ec978b1c
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-06 09:34:17 +0900

    문제 사안
    for (String str1 : s1) 문장은 "향상된 for 루프" 또는 "for-each 루프"라고도 불리는 반복문의 형식입니다. 이 문장은 배열 또는 컬렉션을 순회하면서 각 요소에 접근할 때 사용됩니다.
    
    아래는 for-each 루프의 일반적인 형식입니다:
    
    java
    Copy code
    for (ElementType element : arrayOrCollection) {
        // 반복 실행할 코드
    }
    여기서 ElementType은 배열 또는 컬렉션의 요소 타입을 나타내며, arrayOrCollection은 순회할 배열이나 컬렉션입니다. element는 각 요소를 나타내는 변수로 사용됩니다.
    
    실제로 for-each 루프는 순회할 배열 또는 컬렉션의 크기에 따라 내부적으로 인덱스를 사용하여 요소에 접근하므로, 개발자가 직접 인덱스를 다루지 않아도 됩니다.
    
    예를 들어, 다음과 같이 for-each 루프를 사용하여 배열의 요소에 접근할 수 있습니다
    
    str1.equals(str2)와 str1 == str2는 서로 다른 비교 방식입니다.
    
    str1.equals(str2)는 문자열의 내용을 비교하는 메소드입니다. 문자열의 내용이 동일한 경우에 true를 반환하고, 그렇지 않은 경우에는 false를 반환합니다. 이 비교 방식은 문자열 객체의 내용을 비교하기 위해 사용됩니다.
    
    str1 == str2는 두 개의 문자열 객체의 참조를 비교하는 방식입니다. 즉, 두 개의 문자열 객체가 동일한 메모리 위치를 참조하는지를 확인합니다. 따라서 이 비교 방식은 문자열 객체의 동일성을 확인할 때 사용됩니다.
    
    따라서, 문자열의 내용을 비교하려면 str1.equals(str2)를 사용해야 합니다. 예를 들어:
    
    String str1 = "hello";
    String str2 = "hello";
    String str3 = new String("hello");
    
    System.out.println(str1.equals(str2));  // true
    System.out.println(str1.equals(str3));  // true
    
    System.out.println(str1 == str2);  // true
    System.out.println(str1 == str3);  // false

--- src/_19_배열의유사도.java
+++ src/_19_배열의유사도.java
@@ -1,29 +1,25 @@
 class Solution19 {
     public int solution(String[] s1, String[] s2) {
+        int count = 0; // 일치하는 문자열 개수를 저장할 변수 count를 초기화합니다.
 
-        int count = 0;
-        for (int i = 0; i < s1.length; i++)
-            for (int j = 0; j < s2.length; j++) {
-
-            if(s1==s2)
-                count += 1;
-            else
-                count = 0;
+        for (String str1 : s1) { // s1 배열의 각 문자열에 대해서 반복합니다.
+            for (String str2 : s2) { // s2 배열의 각 문자열에 대해서 반복합니다.
+                if (str1.equals(str2)) { // str1과 str2의 값이 일치하는지 비교합니다.
+                    count++; // 일치하는 경우 count를 증가시킵니다.
+                }
+            }
         }
 
-        return count ;
-
-
+        return count; // 일치하는 문자열 개수를 반환합니다.
     }
 
     public static void main(String[] args) {
-        Solution19 s = new Solution19();
-        String[] s1	= {"a", "b", "c" };
-        String[] s2	= {"com", "b", "d", "p", "c"};
-        int result1 = s.solution(count);
+        Solution19 s = new Solution19(); // Solution19 클래스의 인스턴스 s를 생성합니다.
+        String[] s1 = {"a", "b", "c"}; // 문자열 배열 s1을 초기화합니다.
+        String[] s2 = {"com", "b", "d", "p", "c"}; // 문자열 배열 s2를 초기화합니다.
+        int result1 = s.solution(s1, s2); // solution 메소드를 호출하여 일치하는 문자열 개수를 계산합니다.
 
-
-
+        System.out.println("Matched count: " + result1); // 일치하는 문자열 개수를 출력합니다.
     }
 }
 
