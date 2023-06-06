c8a08ff18be90abc4930eb7867e6c8dd50237b7c
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-06 17:23:32 +0900

    수정 사안
     if (sides[i] + sides[j] <= sides[k] || sides[i] + sides[k] <= sides[j] || sides[j] + sides[k] <= sides[i]) 부분에서 모든 가능한 조합에 대해 삼각형 조건을 판단합니다. 세 변 중 어떤 두 변의 길이의 합이 나머지 한 변의 길이보다 작거나 같다면, 삼각형 조건을 만족하지 않는 것으로 판단하여 1을 반환합니다.
    
    수정된 코드를 실행하면 삼각형 조건을 만족하지 않는 경우에는 1이 반환되고, 모든 경우에 삼각형 조건을 만족하는 경우에는 3이 반환될 것입니다.

--- src/_21_삼각형의완성조건.java
+++ src/_21_삼각형의완성조건.java
@@ -3,8 +3,11 @@ class Solution21 {
         for (int i = 0; i < sides.length; i++) {
             for (int j = 0; j < sides.length; j++) {
                 for (int k = 0; k < sides.length; k++) {
-                    if (sides[i] + sides[j] <= sides[k])  // 삼각형 조건을 만족하지 않는 경우
-                        return 1;
+                    if (i != j && i != k && j != k) {  // 서로 다른 세 변을 선택하여 조건 판단
+                        if (sides[i] + sides[j] <= sides[k] || sides[i] + sides[k] <= sides[j] || sides[j] + sides[k] <= sides[i]) {
+                            return 1;  // 삼각형 조건을 만족하지 않는 경우
+                        }
+                    }
                 }
             }
         }
@@ -14,7 +17,7 @@ class Solution21 {
 
     public static void main(String[] args) {
         Solution21 s = new Solution21();
-        int[] sides1 = {2, 8, 15};
+        int[] sides1 = {4, 13, 15};
         int result1 = s.solution(sides1);
         System.out.println(result1);
     }
