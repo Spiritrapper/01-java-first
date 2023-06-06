0ea3021e6688c8a9938fb8e8959609c1418b6a54
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-06 11:23:35 +0900

    문제 사안
    6번째 줄: count 변수를 선언하고 0으로 초기화합니다. 이 변수는 일치하는 요소의 개수를 저장하기 위해 사용됩니다.
    8번째 줄: 배열 array의 요소를 하나씩 확인하기 위한 반복문을 시작합니다.
    10번째 줄: 내부에 또 다른 반복문이 있지만, 비어 있어서 아무런 동작을 하지 않습니다.
    13번째 줄: array[i]와 array[j]를 비교하여 요소가 일치하는지 확인합니다.
    14번째 줄: 일치하는 경우, count 변수를 증가시킵니다.
    17번째 줄: 모든 반복이 완료된 후, count 변수를 반환합니다.
    21번째 줄: Solution20 클래스의 인스턴스 s를 생성합니다.
    23번째 줄: 주어진 입력 배열 array1을 초기화합니다.
    24번째 줄: s.solution(array1)을 호출하여 일치하는 요소 개수를 계산합니다.
    27번째 줄: 결과를 출력합니다.

--- src/_20_중복숫자개수.java
+++ src/_20_중복숫자개수.java
@@ -1,24 +1,29 @@
 class Solution20 {
-    public int solution(int[] array, int n) {
-        int count = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
+    public int solution(int[] array) {
+        int count = 0;  // 일치하는 요소 개수를 저장하기 위한 count 변수 초기화
+
+        for (int i = 0; i < array.length; i++) {  // 배열의 요소를 하나씩 확인하기 위한 반복문
+            for (int j = 0; j < array.length; j++) {  // 다른 요소들과 비교하기 위한 반복문
+                // 비교할 요소가 없으므로 비어 있는 반복문
             }
-            if(int[i] == int[j])
-                count++;
 
-        }return count;
+            if (int[i] == int[j])  // 배열 요소를 비교하여 일치하는지 확인
+            count++;  // 일치하는 경우 count 변수 증가
+        }
 
+        return count;  // 일치하는 요소 개수 반환
     }
 
     public static void main(String[] args) {
-        Solution20 s = new Solution20();
-        int[] array1 ={1, 1, 3, 3, 13};
-        int result1 = s.solution(int[] array1);
+        Solution20 s = new Solution20();  // Solution20 클래스의 인스턴스 생성
 
+        int[] array1 = {1, 1, 3, 3, 13};  // 주어진 입력 배열 초기화
+        int result1 = s.solution(int[] array1);  // solution 메소드 호출하여 일치하는 요소 개수 계산
+
+        // 결과 출력
+        System.out.println("Count: " + result1);
     }
 }
-
 
 
 public class _20_중복숫자개수 {
