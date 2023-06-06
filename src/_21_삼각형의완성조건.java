6307c873edcb0e1cc6bd20bdf077da403404fe18
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-06 17:29:42 +0900

    수정 사안
    주어진 세 변의 길이를 오름차순으로 정렬한 후, 가장 긴 변의 길이(sides[2])가 나머지 두 변의 길이의 합(sides[0] + sides[1])보다 작다면 삼각형을 완성할 수 있습니다. 이 경우 1을 반환합니다. 그렇지 않은 경우 삼각형을 완성할 수 없으므로 2를 반환합니다.
    
    주어진 예시에 대해 위의 코드를 실행하면 주어진 입력에 따라 올바른 결과가 출력됩니다

--- src/_21_삼각형의완성조건.java
+++ src/_21_삼각형의완성조건.java
@@ -1,25 +1,32 @@
+import java.util.Arrays;
 class Solution21 {
     public int solution(int[] sides) {
-        for (int i = 0; i < sides.length; i++) {
-            for (int j = 0; j < sides.length; j++) {
-                for (int k = 0; k < sides.length; k++) {
-                    if (i != j && i != k && j != k) {  // 서로 다른 세 변을 선택하여 조건 판단
-                        if (sides[i] + sides[j] <= sides[k] || sides[i] + sides[k] <= sides[j] || sides[j] + sides[k] <= sides[i]) {
-                            return 1;  // 삼각형 조건을 만족하지 않는 경우
-                        }
-                    }
-                }
-            }
-        }
+        // 세 변의 길이를 오름차순으로 정렬
+        Arrays.sort(sides);
 
-        return 3;  // 모든 경우에 삼각형 조건을 만족하는 경우
+        int maxSide = sides[2];  // 가장 긴 변의 길이
+        int sumOfOtherSides = sides[0] + sides[1];  // 나머지 두 변의 길이의 합
+
+        if (maxSide < sumOfOtherSides) {
+            return 1;  // 삼각형을 완성할 수 있는 경우
+        } else {
+            return 2;  // 삼각형을 완성할 수 없는 경우
+        }
     }
 
     public static void main(String[] args) {
         Solution21 s = new Solution21();
-        int[] sides1 = {4, 13, 15};
+        int[] sides1 = {1, 2, 3};
         int result1 = s.solution(sides1);
-        System.out.println(result1);
+        System.out.println(result1);  // 2
+
+        int[] sides2 = {3, 6, 2};
+        int result2 = s.solution(sides2);
+        System.out.println(result2);  // 2
+
+        int[] sides3 = {199, 72, 222};
+        int result3 = s.solution(sides3);
+        System.out.println(result3);  // 1
     }
 }
 
