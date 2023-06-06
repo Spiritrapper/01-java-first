a2808ab52ae96dfcc67aeb10ac3cbc795cb58c1d
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-06 11:21:19 +0900

    문제 사안
    배열 요소를 비교할 때 int[i]와 int[j]를 사용하고 있습니다. 올바른 문법은 array[i]와 array[j]입니다. 배열 이름으로 array를 사용하셔야 합니다.
    
    int[] array1 = {1, 1, 3, 3, 13};과 같이 배열을 초기화할 때 int[]를 제거해야 합니다. 즉, int[] array1 대신에 array1로 사용해야 합니다.
    
    int result1 = s.solution(int[] array1);에서 배열을 인자로 전달할 때 int[]를 제거해야 합니다. 즉, s.solution(array1)로 사용해야 합니다.

---
+++ src/_20_중복숫자개수.java
@@ -0,0 +1,25 @@
+class Solution20 {
+    public int solution(int[] array, int n) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+            }
+            if(int[i] == int[j])
+                count++;
+
+        }return count;
+
+    }
+
+    public static void main(String[] args) {
+        Solution20 s = new Solution20();
+        int[] array1 ={1, 1, 3, 3, 13};
+        int result1 = s.solution(int[] array1);
+
+    }
+}
+
+
+
+public class _20_중복숫자개수 {
+}
