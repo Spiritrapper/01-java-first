29062e0ada6883f6848857c85522cb708398bac5
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-07 13:55:49 +0900

    수정 사안
    Arrays.sort(array);를 사용하여 주어진 배열 array를 오름차순으로 정렬합니다.
    count 변수는 현재 연속해서 등장하는 개수를 저장합니다.
    maxCount 변수는 최대 연속 개수를 저장합니다. 초기값은 0으로 설정됩니다.
    for 반복문을 사용하여 배열의 각 원소를 순회합니다.
    array[i]가 height보다 작으면 count를 1 증가시킵니다. 이는 연속해서 등장하는 개수를 세는 역할을 합니다.
    동시에 maxCount를 Math.max() 함수를 사용하여 현재 count와 기존의 maxCount 중 더 큰 값을 선택하여 업데이트합니다. 이를 통해 최대 연속 개수를 찾습니다.
    array[i]가 height보다 크거나 같은 경우에는 count를 0으로 초기화합니다. 이는 연속해서 등장하는 개수를 중단하고 다시 시작하는 역할을 합니다.
    반복문이 종료되면 최종적으로 maxCount 변수에는 최대 연속 개수가 저장됩니다.
    maxCount 값을 반환하여 최대 연속 개수를 출력합니다.

--- src/_26_머쓱이보다키큰사람.java
+++ src/_26_머쓱이보다키큰사람.java
@@ -1,23 +1,29 @@
 import java.util.Arrays;
+
 class Solution26 {
     public int solution(int[] array, int height) {
-        Arrays.sort(array);
-        int count = 0;
+        Arrays.sort(array); // 배열을 오름차순으로 정렬
+
+        int count = 0; // 현재 연속 개수를 저장하는 변수
+        int maxCount = 0; // 최대 연속 개수를 저장하는 변수
+
         for (int i = 0; i < array.length; i++) {
-            if(array[i] < height)
-                count+= 1;
-            else
-                count = 0;
+            if (array[i] < height) {
+                count += 1; // 특정 높이보다 작은 원소를 만나면 count를 1 증가
+                maxCount = Math.max(maxCount, count); // 최대 연속 개수 업데이트
+            } else {
+                count = 0; // 특정 높이보다 크거나 같은 원소를 만나면 count를 0으로 초기화
+            }
         }
-        return count;
+
+        return maxCount; // 최대 연속 개수 반환
     }
 
     public static void main(String[] args) {
         Solution26 s = new Solution26();
-        int [] array1 = {149, 180, 192, 170};
+        int[] array1 = {149, 180, 192, 170};
         int result1 = s.solution(array1, 167);
         System.out.println(result1);
-
     }
 }
 
