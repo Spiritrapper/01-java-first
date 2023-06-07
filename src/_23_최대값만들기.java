f3bef29e736da19f4bd86f9f720d004f0f294353
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-07 10:13:04 +0900

    문제 사안
    발생한 ArrayIndexOutOfBoundsException 예외는 배열 인덱스가 범위를 벗어나서 발생한 것입니다.
    
    문제는 numbers 배열의 인덱스 접근 부분에 있습니다. 배열의 인덱스는 0부터 시작하기 때문에 numbers.length는 배열의 길이보다 1 큰 값을 가집니다. 따라서, 배열의 마지막 원소에 접근하기 위해서는 numbers.length - 1을 사용해야 합니다.

---
+++ src/_23_최대값만들기.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+class Solution23 {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        int answer = numbers[numbers.length] * numbers[numbers.length-1];
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution23 s = new Solution23();
+        int[] numbers1 = {3, 1, 5, 2, 4};
+        int result1 = s.solution(numbers1);
+        System.out.println(result1);
+    }
+
+}
+
+
+public class _23_최대값만들기 {
+}
