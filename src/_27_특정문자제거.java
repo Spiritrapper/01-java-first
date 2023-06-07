b8939002d24b09ea18c26afdd383e5dac38c96cc
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-07 17:21:45 +0900

    수정 사안
    contains와 append 메서드는 모두 문자열에서 사용할 수 있는 메서드입니다.
    
    contains: 문자열 메서드
    
    contains 메서드는 문자열에서 지정한 문자 또는 문자열이 포함되어 있는지를 확인하는 메서드입니다.
    사용법: 문자열.contains(문자 또는 문자열)
    반환값: 문자열이 포함되어 있으면 true, 포함되어 있지 않으면 false를 반환합니다.
    예시: "Hello".contains("H")는 true를 반환합니다.
    append: 문자열 또는 StringBuilder 객체의 메서드
    
    append 메서드는 문자열 또는 StringBuilder 객체에 문자열을 추가하는 메서드입니다.
    문자열에 사용할 경우, append 메서드는 새로운 문자열을 생성하고 반환합니다.
    StringBuilder 객체에 사용할 경우, append 메서드는 기존 문자열에 문자열을 추가하여 변경된 StringBuilder 객체를 반환합니다.
    사용법:
    문자열: "문자열1".append("문자열2")
    StringBuilder: StringBuilder객체.append("문자열")
    반환값: 문자열 또는 변경된 StringBuilder 객체
    예시:
    "Hello,".append(" World!")는 "Hello, World!"를 생성하여 반환합니다.
    StringBuilder sb = new StringBuilder("Hello,"); sb.append(" World!");는 sb 객체의 문자열을 "Hello, World!"로 변경합니다.
    
    new StringBuilder()은 빈 문자열을 가진 새로운 StringBuilder 객체를 생성합니다. result 변수에 이 객체의 참조를 할당하여 사용할 수 있게 됩니다.
    
    StringBuilder 객체는 기존의 문자열에 새로운 문자열을 추가하거나 기존 문자열을 수정하는 등의 작업을 수행할 때 유용합니다. 문자열을 append 메서드를 사용하여 추가하면 기존 문자열에 연결됩니다. 이러한 특성은 문자열을 여러 번 수정할 때 매번 새로운 문자열을 생성하는 것보다 효율적입니다. 이는 문자열을 연결할 때마다 새로운 문자열 객체를 생성하는 + 연산자와는 대조적입니다.
    
    예를 들어, 다음은 StringBuilder를 사용하여 문자열을 추가하는 예시입니다:
    
    java
    Copy code
    StringBuilder sb = new StringBuilder();
    sb.append("Hello, ");
    sb.append("World!");
    String result = sb.toString();
    System.out.println(result);
    위 코드는 "Hello, "와 "World!"라는 두 개의 문자열을 StringBuilder를 사용하여 하나의 문자열로 연결하고, 그 결과를 출력합니다. StringBuilder 객체를 사용하면 문자열을 효율적으로 처리할 수 있으며, 반복적인 문자열 조작 작업에 유용합니다

--- src/_27_특정문자제거.java
+++ src/_27_특정문자제거.java
@@ -1,28 +1,35 @@
 import java.util.Arrays;
+
 class Solution27 {
     public String solution(String my_string, String letter) {
+        String[] words = my_string.split("");
+        // 주어진 문자열을 빈 문자열("")을 기준으로 문자 단위로 분리하여 words 배열에 저장합니다.
+
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < my_string.length(); i++) {
-            char c = my_string.charAt(i);
-            if (c == letter.charAt(0)) {
-                continue;
+        // 결과를 저장하기 위한 StringBuilder 객체를 생성합니다.
+
+        for (String word : words) {
+            // words 배열을 순회하면서 각 문자를 word 변수에 담아 반복합니다.
+
+            if (!word.contains(letter)) {
+                // 만약 word 변수에 저장된 문자가 letter 문자를 포함하지 않는다면 실행합니다.
+
+                result.append(word);
+                // word 문자를 결과 문자열인 result에 추가합니다.
+                // 여기서 ""을 추가하는 이유는 문자 단위로 분리되었기 때문에 원래 문자열의 구분을 위해 빈 문자열을 추가합니다.
             }
-            result.append(c);
         }
+
         return result.toString();
+        // 결과 문자열인 result를 문자열로 변환하고 양쪽 공백을 제거한 후 반환합니다.
     }
 
     public static void main(String[] args) {
         Solution27 s = new Solution27();
-        String my_string1 = "abcdef";
-        String letter1 = "f";
+        String my_string1 = "love life long thought";
+        String letter1 = "l";
         String result1 = s.solution(my_string1, letter1);
-        System.out.println(result1);  // 출력: "abcde"
-
-        String my_string2 = "BCBdbe";
-        String letter2 = "B";
-        String result2 = s.solution(my_string2, letter2);
-        System.out.println(result2);  // 출력: "Cdbe"
+        System.out.println(result1);
     }
 }
 
