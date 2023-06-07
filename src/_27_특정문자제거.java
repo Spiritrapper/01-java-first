470955da607d9ffc66d562c8a05d3031e0a019f2
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-07 16:45:40 +0900

    문제 사안
    my_string.split("")과 my_string.split("") 차이점 인식

--- src/_27_특정문자제거.java
+++ src/_27_특정문자제거.java
@@ -1,28 +1,35 @@
 import java.util.Arrays;
+
 class Solution27 {
     public String solution(String my_string, String letter) {
         String[] words = my_string.split(" ");
-        for (int i = 0; i < words.length; i++) {
-            if (words[i] != letter) {
-                words[i] ;
-
+        StringBuilder result = new StringBuilder();
+        for (String word : words) {
+            if (!word.contains(letter)) {
+                result.append(word).append(" ");
             }
-            else
-                break;
         }
-        return String.join(" ", words);
+        return result.toString().trim();
+    }
 
-        }
     public static void main(String[] args) {
         Solution27 s = new Solution27();
-        String my_string1 = "love life long toutht";
+        String my_string1 = "love life long thought";
         String letter1 = "l";
         String result1 = s.solution(my_string1, letter1);
-        System.out.println(result1 );
-
+        System.out.println(result1);
+    }
 }
+
 
 
 
 public class _27_특정문자제거 {
 }
+
+
+
+
+
+
+
