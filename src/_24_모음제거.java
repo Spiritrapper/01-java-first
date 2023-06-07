546345b3cb6bee02b5e187b3a9fca2c2e059fd7a
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-07 11:42:20 +0900

    수정 사안
    Solution 클래스: 주어진 문자열에서 모음을 제거하는 문제를 해결하는 클래스입니다.
    solution 메소드: 주어진 문자열을 반복하면서 모음을 제외한 문자를 결과 문자열에 추가한 후, 최종 결과 문자열을 반환합니다.
    StringBuilder 객체: 결과 문자열을 저장하기 위한 StringBuilder 객체(result)를 생성합니다.
    for 문: 주어진 문자열을 반복하면서 각 문자에 대해 처리를 수행합니다.
    charAt 메소드: 문자열에서 해당 인덱스의 문자를 추출하여 변수 c에 저장합니다.
    isVowel 메소드: 주어진 문자가 모음인지 확인하는 메소드입니다. 'a', 'e', 'i', 'o', 'u' 중 하나인 경우 true를 반환하고, 그렇지 않으면 false를 반환합니다.
    if 문: 추출한 문자 c가 모음이 아닌 경우에는 결과 문자열(result)에 해당 문자를 추가합니다.
    toString 메소드: StringBuilder 객체(result)를 String으로 변환하여 최종 결과 문자열을 얻습니다.
    main 메소드: Solution 클래스의 인스턴스를 생성하고, 주어진 예시에 대해 solution 메소드를 호출하여 결과를 출력합니다
    
    수정 보충내용
    
    !isVowel, append(c), charAt(i)는 자바 내장 함수 및 연산자입니다. 각각의 역할은 다음과 같습니다:
    
    !isVowel(c): isVowel(c) 메소드의 반환값을 논리 부정 연산자 !를 통해 반대로 만듭니다. isVowel(c)가 true이면 !isVowel(c)는 false가 되고, isVowel(c)가 false이면 !isVowel(c)는 true가 됩니다.
    append(c): StringBuilder 객체에 문자 c를 추가합니다. append() 메소드는 StringBuilder 객체의 끝에 문자열이나 문자를 추가하는 역할을 합니다.
    charAt(i): 문자열에서 인덱스 i에 해당하는 위치에 있는 문자를 추출합니다. charAt() 메소드는 문자열의 특정 인덱스에 있는 문자를 반환합니다.
    이러한 함수 및 연산자는 자바에서 문자열 처리, 문자열 조작 등 다양한 작업에 사용됩니다.

--- src/_24_모음제거.java
+++ src/_24_모음제거.java
@@ -1,28 +1,36 @@
-import java.util.Arrays;
-
 class Solution24 {
     public String solution(String my_string) {
-        String[] words = my_string.split(" "); // 주어진 문자열을 공백을 기준으로 분리하여 단어 배열 생성
+        StringBuilder result = new StringBuilder(); // 결과를 저장할 StringBuilder 객체 생성
 
-        for (int i = 0; i < words.length; i++) {
-            words[i] = removeVowels(words[i]); // 모음을 제거한 단어로 대체
+        // 주어진 문자열을 반복하면서 모음을 제외한 문자를 결과에 추가
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i); // 문자열에서 해당 인덱스의 문자 추출
+            if (!isVowel(c)) { // 추출한 문자가 모음이 아닌 경우
+                result.append(c); // 결과 문자열에 해당 문자 추가
+            }
         }
 
-        return String.join(" ", words); // 단어들을 공백을 포함하여 연결하여 최종 결과 문자열 반환
+        return result.toString(); // StringBuilder 객체를 String으로 변환하여 결과 문자열 반환
     }
 
-    private String removeVowels(String word) {
-        return word.replaceAll("[aeiouAEIOU]", ""); // 정규식을 사용하여 모음을 제거한 문자열 반환
+    // 문자가 모음인지 확인하는 메소드
+    private boolean isVowel(char c) {
+        // 주어진 문자가 'a', 'e', 'i', 'o', 'u' 중 하나인지 확인
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 
     public static void main(String[] args) {
-        Solution24 s = new Solution24();
-        String my_string1 = "nice to meet you";
-        String result1 = s.solution(my_string1);
-        System.out.println(result1); // 최종 결과 문자열 출력
+        Solution24 solution = new Solution24(); // Solution 객체 생성
+
+        String my_string1 = "bus";
+        String result1 = solution.solution(my_string1);
+        System.out.println(result1); // "bs"
+
+        String my_string2 = "nice to meet you";
+        String result2 = solution.solution(my_string2);
+        System.out.println(result2); // "nc t mt y"
     }
 }
-
 
 public class _24_모음제거 {
 }
