75243eb06914488427afe4e657159f060ae495ee
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-07 18:44:58 +0900

    문제 사안
    solution 메서드 내에서 int[] answer 배열을 생성하고, 각 요소를 2배로 변환하여 저장합니다.
    반복문이 종료되고 나서 answer 배열을 반환하도록 return answer; 문장을 추가합니다.
    main 메서드에서 System.out.println(Arrays.toString(result));을 사용하여 결과 배열을 출력합니다. 배열은 Arrays.toString() 메서드를 사용하여 문자열로 변환하여 출력해야 합니다.

---
+++ src/_30_배열두배만들기.java
@@ -0,0 +1,26 @@
+
+class Solution30 {
+    public int[] solution(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+             int answer = numbers[i] * 2;
+
+        }
+        return answer;
+
+
+    }
+
+    public static void main(String[] args) {
+        Solution30 s = new Solution30();
+        int[] numbers1 = {1, 2, 3, 4, 5};
+        int[] result = s.solution(numbers1);
+        System.out.println(result);
+    }
+}
+
+
+
+
+
+public class _30_배열두배만들기 {
+}
