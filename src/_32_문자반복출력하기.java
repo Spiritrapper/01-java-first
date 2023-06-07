00c8cc7a40395d4af15f54588c8d1baff924b3a5
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-08 08:43:23 +0900

    문제 사안
    결과를 저장하기 위한 StringBuilder 객체를 생성해야한다.

---
+++ src/_32_문자반복출력하기.java
@@ -0,0 +1,34 @@
+class Solution32 {
+    public String solution(String my_string, int n) {
+        char[] character = my_string.toCharArray();
+        for (int i = 0; i < n; i++) {
+            result.append(character);
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution32 s = new Solution32();
+        String myString = "hello";
+        int n = 3;
+        String result = s.solution(myString, n);
+        System.out.println(result);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public interface _32_문자반복출력하기 {
+}
