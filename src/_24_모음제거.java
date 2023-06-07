73da5f5e89b69f9dd5e50afac3c1d3581ea2f4e7
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-07 10:38:17 +0900

    문제 사안
    String [] my_string = String my_string;이 부분은 잘못된 문법입니다. 변수 my_string을 배열로 선언하는 부분인데, 이미 my_string은 문자열 타입으로 선언되었기 때문에 배열로 선언할 필요가 없습니다. 올바른 코드는 String[] words = my_string.split(" ");입니다. 이렇게 하면 주어진 문자열을 공백을 기준으로 분리하여 단어들의 배열을 생성할 수 있습니다.
    
    my_string[i]== a || e || i || o || u는 잘못된 조건식입니다. 문자열 내의 문자를 비교할 때는 작은따옴표(' ')를 사용하여 문자를 표현해야 합니다. 또한 여러 조건을 동시에 비교하기 위해서는 각각의 조건을 별도로 작성해야 합니다. 수정된 코드는 if (my_string[i] == 'a' || my_string[i] == 'e' || my_string[i] == 'i' || my_string[i] == 'o' || my_string[i] == 'u')입니다.
    
    else String my_string;은 잘못된 구문입니다. else 뒤에는 조건 없이 바로 실행되는 블록이어야 합니다. 변수 my_string을 다시 선언하는 것은 의미가 없으므로 삭제해야 합니다.
    
    return "my_string";은 항상 문자열 "my_string"을 반환하게 되므로, 실제 처리한 결과가 아닙니다. 모음을 제거한 문자열을 반환해야 합니다. 수정된 코드는 return String.join("", words);입니다. 이렇게 하면 단어들의 배열을 공백 없이 연결하여 최종 결과 문자열을 반환할 수 있습니다.

---
+++ src/_24_모음제거.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+class Solution24 {
+    public String solution(String my_string) {
+        String [] my_string = String my_string;
+        for (int i = 0; i < my_string.length; i++) {
+            if(my_string[i]== a || e || i || o || u)
+                return "";
+            else String my_string;
+
+        } return "my_string";
+    }
+
+    public static void main(String[] args) {
+        Solution24 s = new Solution24();
+        String my_string1 = "nice to meet you";
+        String result1 = s.solution(my_string1);
+        System.out.println(result1);
+    }
+}
+
+
+
+public class _24_모음제거 {
+}
