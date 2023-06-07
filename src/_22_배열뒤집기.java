79e12d75b8ba03eff2dd1f5c7b3f2bbd31638f3a
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-07 09:44:33 +0900

    수정 사안
    int n = num_list.length;: 주어진 배열 num_list의 길이를 저장하는 변수 n을 선언합니다.
    
    for (int i = 0; i < n / 2; i++) {: 배열을 뒤집기 위한 반복문을 시작합니다. 배열의 반절까지만 반복하면 모든 원소를 대칭적으로 교환할 수 있습니다.
    
    int temp = num_list[i];: 현재 위치 i의 값을 임시 변수 temp에 저장합니다.
    
    num_list[i] = num_list[n - i - 1];: 대칭 위치인 n - i - 1의 값을 현재 위치 i로 이동시킵니다.
    
    num_list[n - i - 1] = temp;: 임시 변수 temp의 값을 대칭 위치로 이동시킵니다.
    
    return num_list;: 뒤집힌 배열 num_list를 반환합니다.
    
    int[] num_list1 = {1, 2, 3, 4, 5};: 주어진 예시 배열 num_list1을 선언하고 초기화합니다.
    
    int[] result1 = s.solution(num_list1);: num_list1을 인자로하여 solution 메서드를 호출하고 반환된 뒤집힌 배열을 result1에 저장합니다.
    
    for (int num : result1) {: 뒤집힌 배열 result1의 각 원소에 대해서 반복합니다.
    
    System.out.print(num + " ");: 각 원소를 출력합니다.

--- src/_22_배열뒤집기.java
+++ src/_22_배열뒤집기.java
@@ -1,23 +1,24 @@
 class Solution22 {
     public int[] solution(int[] num_list) {
-        for (int i = 0; i < num_list.length; i++) {
-            for (int j = 0; j < num_list.length; j++) {
-                num_list[i] = num_list[num_list.length-j];
+        int n = num_list.length;
+        for (int i = 0; i < n / 2; i++) {  // 배열의 반절까지만 반복
+            int temp = num_list[i];  // 현재 위치의 값을 임시 변수에 저장
+            num_list[i] = num_list[n - i - 1];  // 대칭 위치의 값을 현재 위치로 이동
+            num_list[n - i - 1] = temp;  // 임시 변수의 값을 대칭 위치로 이동
+        }
 
-            }
-        }int[] answer = num_list[num_list.length-j];
-
-        return answer;
+        return num_list;  // 뒤집힌 배열을 반환
     }
 
     public static void main(String[] args) {
         Solution22 s = new Solution22();
-        int num_list1 = {1, 2, 3, 4, 5 } ;
+        int[] num_list1 = {1, 2, 3, 4, 5};
+        int[] result1 = s.solution(num_list1);
+        for (int num : result1) {  // 뒤집힌 배열의 원소를 하나씩 출력
+            System.out.print(num + " ");
+        }
     }
 }
-
-
-
 
 public class _22_문자열뒤집기 {
 }
