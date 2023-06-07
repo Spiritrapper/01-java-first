2ab7dbe37108d216ea95db75187e4bc7e4b7bcf0
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-07 19:24:29 +0900

    수정 사안
    int[] answer = new int[numbers.length];는 numbers 배열과 동일한 길이를 갖는 정수 배열을 생성하는 코드입니다. answer 배열은 결과를 저장하기 위한 배열로 사용됩니다. numbers 배열의 각 요소를 2배로 변환한 결과를 answer 배열에 저장하여 반환하는 것이 이 문제의 목표입니다.
    
    예를 들어, numbers 배열이 {1, 2, 3, 4, 5}로 주어진 경우, answer 배열은 {0, 0, 0, 0, 0}로 초기화됩니다. 그 후, 반복문을 통해 numbers 배열의 각 요소를 2배로 변환하여 answer 배열에 저장합니다. 결과적으로 answer 배열은 {2, 4, 6, 8, 10}이 됩니다. 이렇게 변환된 answer 배열을 반환하여 문제의 요구사항을 충족시킵니다.
    
    따라서 int[] answer = new int[numbers.length];는 결과를 저장할 배열을 생성하는 코드입니다. answer 배열의 각 요소는 초기값인 0으로 초기화되며, 반복문을 통해 해당 요소를 변환한 값을 저장합니다. 최종적으로 answer 배열이 반환되어 결과를 나타냅니다.

--- src/_30_배열두배만들기.java
+++ src/_30_배열두배만들기.java
@@ -1,20 +1,20 @@
-
+import java.util.Arrays;
 class Solution30 {
     public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length]; // 결과 배열을 저장할 배열 생성
+
         for (int i = 0; i < numbers.length; i++) {
-             int answer = numbers[i] * 2;
-
+            answer[i] = numbers[i] * 2; // 각 요소를 2배로 변환하여 결과 배열에 저장
         }
-        return answer;
 
-
+        return answer; // 결과 배열을 반환
     }
 
     public static void main(String[] args) {
         Solution30 s = new Solution30();
         int[] numbers1 = {1, 2, 3, 4, 5};
         int[] result = s.solution(numbers1);
-        System.out.println(result);
+        System.out.println(Arrays.toString(result)); // 결과 배열 출력
     }
 }
 
