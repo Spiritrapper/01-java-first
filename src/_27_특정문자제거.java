03e285f2ab1799bafbc43b6e45b8ae9e20971b1f
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-07 16:56:12 +0900

    수정 사안
    이 코드는 문자열을 순회하면서 각 문자가 letter와 같지 않은 경우에만 결과 문자열에 추가하는 방식으로 문자 letter를 제거한 결과를 반환합니다.

--- src/_27_특정문자제거.java
+++ src/_27_특정문자제거.java
@@ -1,27 +1,29 @@
 import java.util.Arrays;
-
 class Solution27 {
     public String solution(String my_string, String letter) {
-        String[] words = my_string.split(" ");
         StringBuilder result = new StringBuilder();
-        for (String word : words) {
-            if (!word.contains(letter)) {
-                result.append(word).append(" ");
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            if (c != letter.charAt(0)) {
+                result.append(c);
             }
         }
-        return result.toString().trim();
+        return result.toString();
     }
 
     public static void main(String[] args) {
         Solution27 s = new Solution27();
-        String my_string1 = "love life long thought";
-        String letter1 = "l";
+        String my_string1 = "abcdef";
+        String letter1 = "f";
         String result1 = s.solution(my_string1, letter1);
-        System.out.println(result1);
+        System.out.println(result1);  // 출력: "abcde"
+
+        String my_string2 = "BCBdbe";
+        String letter2 = "B";
+        String result2 = s.solution(my_string2, letter2);
+        System.out.println(result2);  // 출력: "Cdbe"
     }
 }
-
-
 
 
 public class _27_특정문자제거 {
