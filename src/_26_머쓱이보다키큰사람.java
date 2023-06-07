5347959743364d66037f58013ce170bf176b8ff7
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-07 13:38:11 +0900

    문제 사안
    조건문 로직 오류: count 변수의 초기화와 갱신이 잘못되었습니다. 현재 코드에서는 array[i] < height 조건을 만족하는 경우에만 count를 증가시키고, 그렇지 않은 경우에는 count를 0으로 초기화하고 있습니다. 따라서 array[i] < height 조건을 만족하지 않는 경우에도 count가 0으로 초기화되므로 잘못된 결과가 반환됩니다.
    
    결과값 계산 오류: 현재 코드에서는 count 변수가 array[i] < height 조건을 만족하는 개수를 세는 용도로 사용되고 있지만, 실제로는 array[i] < height 조건을 만족하는 최대 연속 개수를 세어야 합니다. 따라서 count 변수가 아니라 다른 변수를 사용하여 최대 연속 개수를 계산해야 합니다.

---
+++ src/_26_머쓱이보다키큰사람.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+class Solution26 {
+    public int solution(int[] array, int height) {
+        Arrays.sort(array);
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] < height)
+                count+= 1;
+            else
+                count = 0;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Solution26 s = new Solution26();
+        int [] array1 = {149, 180, 192, 170};
+        int result1 = s.solution(array1, 167);
+        System.out.println(result1);
+
+    }
+}
+
+
+
+
+
+public class _26_머쓱이보다키큰사람 {
+}
