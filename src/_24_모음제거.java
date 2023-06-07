5961bc32f91d838ab93a2c796d0155c4efaf56f7
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-07 11:19:41 +0900

    수정 사안
    solution 메서드는 주어진 문자열에서 모음을 제거하고 결과 문자열을 반환하는 역할을 수행합니다.
    my_string.split(" ")은 주어진 문자열을 공백을 기준으로 분리하여 단어들의 배열을 생성합니다.
    for 루프는 배열의 각 단어에 대해서 실행됩니다.
    containsVowel 메서드는 해당 단어에 모음이 포함되어 있는지 확인하는 역할을 수행합니다.
    containsVowel 메서드에서는 단어에 모음(a, e, i, o, u)이 포함되어 있는지 확인하고, 결과를 true 또는 false로 반환합니다.
    만약 단어에 모음이 포함되어 있다면, 해당 단어를 빈 문자열로 대체하여 모음을 제거합니다.
    String.join("", words)는 단어들을 공백 없이 연결하여 최종 결과 문자열을 생성합니다.
    main 메서드에서는 주어진 예시 문자열에 대해 solution 메서드를 호출하고 결과를 출력합니다.
    
    문제 사안
    
    출력이 되지 않는다.

--- src/_24_모음제거.java
+++ src/_24_모음제거.java
@@ -1,24 +1,32 @@
 import java.util.Arrays;
 class Solution24 {
     public String solution(String my_string) {
-        String [] my_string = String my_string;
-        for (int i = 0; i < my_string.length; i++) {
-            if(my_string[i]== a || e || i || o || u)
-                return "";
-            else String my_string;
+        String[] words = my_string.split(" "); // 주어진 문자열을 공백을 기준으로 분리하여 단어들의 배열 생성
 
-        } return "my_string";
+        for (int i = 0; i < words.length; i++) {  // 배열을 만들어야 length의 사용가능
+            if (containsVowel(words[i])) { // 해당 단어에 모음이 포함되어 있는지 확인
+                words[i] = ""; // 모음이 포함된 단어는 빈 문자열로 대체
+            }
+        }
+
+        return String.join("", words); // 단어들을 공백 없이 연결하여 최종 결과 문자열 반환
+    }
+
+    private boolean containsVowel(String word) {
+        return word.contains("a") || word.contains("e") || word.contains("i") || word.contains("o") || word.contains("u");
+
+        // 단어에 모음(a, e, i, o, u)이 포함되어 있는지 확인하는 메서드
     }
 
     public static void main(String[] args) {
         Solution24 s = new Solution24();
         String my_string1 = "nice to meet you";
         String result1 = s.solution(my_string1);
-        System.out.println(result1);
+        System.out.println(s.solution(my_string1));
     }
 }
 
 
-
 public class _24_모음제거 {
 }
+
