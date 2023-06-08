e9bb5d70cf99912000df5a752279b2845c4fcf70
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-08 15:06:36 +0900

    수정 사안
    6번 줄: n 변수 제거
    11번 줄: !words[i].equals("")로 문자열이 비어있는지 확인
    13-17번 줄: 숫자로 변환 가능한지 확인하여 sum에 더함. 변환할 수 없는 경우 NumberFormatException 예외를 처리하여 0을 반환
    21번 줄: 반복문 종료 후에 sum 반환
    
    문제 사안
    
    11번 줄: 문자열이 비어있는지 확인하는 조건문을 !words[i].equals("")에서 !words[i].equals(" ")로 변경해야 합니다. 즉, 빈 문자열 대신 공백 문자열로 비교해야 합니다.
    
    13번 줄: NumberFormatException 예외가 발생하는 경우 return 0; 대신에 sum += 0;으로 수정해야 합니다. 즉, 숫자로 변환할 수 없는 경우에도 합산을 진행해야 합니다.

--- src/_34_숨어있는숫자의덧셈.java
+++ src/_34_숨어있는숫자의덧셈.java
@@ -1,14 +1,17 @@
 class Solution34 {
-    public int solution(String my_string){
+    public int solution(String my_string) {
         int sum = 0;
-        int n =0;
         String[] words = my_string.split("");
+
         for (int i = 0; i < words.length; i++) {
-            if(words[i] != n ){
-                return 0;
-            }else
-                return words[i];
-            sum += words[i];
+            if (!words[i].equals("")) {
+                try {
+                    int n = Integer.parseInt(words[i]);
+                    sum += n;
+                } catch (NumberFormatException e) {
+                    return 0;
+                }
+            }
         }
         return sum;
     }
@@ -23,10 +26,8 @@ class Solution34 {
         String my_string2 = "1a2b3c4d123";
         int result2 = solution.solution(my_string2);
         System.out.println(result2); // 16
-
     }
 }
-
 
 
 
