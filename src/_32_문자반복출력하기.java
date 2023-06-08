f88343ecb2bd9b7783132f99cde6a8de4fa2f03f
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-08 09:04:36 +0900

    수정 사안
    for 문은 주어진 문자열 myString의 각 문자를 반복하는 역할을 합니다. 코드 내에서 for (int i = 0; i < myString.length(); i++) 부분이 해당 반복문을 정의하고 있습니다.
    
    구체적으로 설명하자면, i 변수는 0부터 시작하여 myString.length() - 1까지 반복하며, i는 인덱스로 사용됩니다. 이렇게 반복하면서 myString.charAt(i)를 통해 myString의 각 문자에 접근할 수 있습니다.
    
    예를 들어, "hello"라는 문자열이 주어진 경우, for 문은 h, e, l, l, o의 순서로 해당 문자들을 반복하여 처리합니다. 이러한 반복을 통해 result StringBuilder에 문자를 추가하고 최종적으로 반환될 문자열을 생성합니다.
    
    User
     hhheeelllooo  hellohellohello차이점은 어떻게 생기는거야?
    ChatGPT
    hhheeelllooo와 hellohellohello는 myString의 각 문자를 n번 반복하여 생성한 문자열입니다. 그러나 두 개의 코드에서 차이가 발생한 이유는 다음과 같습니다:
    
    첫 번째 코드:
    
    for (char ch : myString.toCharArray())를 통해 myString의 각 문자를 반복합니다.
    내부 for 문에서는 n번 반복하여 result에 문자를 추가합니다.
    결과적으로 myString의 각 문자를 n번 반복한 문자열이 생성됩니다.
    두 번째 코드:
    
    for (int i = 0; i < myString.length(); i++)를 통해 myString의 각 문자에 접근합니다.
    내부 for 문에서는 n번 반복하여 result에 문자를 추가합니다.
    결과적으로 myString의 각 문자를 순서대로 n번 반복한 문자열이 생성됩니다.
    따라서, 첫 번째 코드에서는 myString의 각 문자를 순서대로 n번 반복하고, 두 번째 코드에서는 myString의 각 문자를 순서대로 n번 반복한 결과가 생성됩니다. 이로 인해 두 문자열은 서로 다른 값을 갖게 됩니다.

--- src/_32_문자반복출력하기.java
+++ src/_32_문자반복출력하기.java
@@ -1,22 +1,36 @@
+import java.lang.String;
 class Solution32 {
-    public String solution(String my_string, int n) {
+    public String solution(String myString, int n) {
         StringBuilder result = new StringBuilder();
-        char[] character = my_string.toCharArray();
-        for (int i = 0; i < n; i++) {
-            result.append(character);
+        // 빈 StringBuilder 객체 생성
+
+        for (int i = 0; i < myString.length(); i++) {
+            // 문자열 길이만큼 반복
+//            for (int j = 0; j < n; j++) {
+                // n번 반복
+                result.append(myString.charAt(i));
+                // 문자를 StringBuilder에 추가
+//            }
         }
         return result.toString();
+        // StringBuilder를 문자열로 변환하여 반환
     }
 
     public static void main(String[] args) {
         Solution32 s = new Solution32();
+        // Solution32 객체 생성
+
         String myString = "hello";
         int n = 3;
+        // 입력 값 설정
+
         String result = s.solution(myString, n);
+        // solution 메서드 호출하여 결과 값 저장
+
         System.out.println(result);
+        // 결과 값 출력
     }
 }
-
 
 
 
