3c8ecebd9d2d177d51cbef20e320f3c4c98a896a
spiritrapper <134787232+spiritrapper@users.noreply.github.com>
2023-06-08 19:03:32 +0900

    수정 사안
     rsp 문자열을 순회하면서 각 문자에 대해 조건에 따라 결과 문자열 result에 문자를 추가합니다. 연결 연산자인 +를 사용하여 문자열을 연결하고 최종적으로 result를 반환

--- src/_39_가위바위보.java
+++ src/_39_가위바위보.java
@@ -1,20 +1,17 @@
 class Solution39 {  // 가위바위보 숫자된 문자열의 대응 위치 : for문을 통해 각문자를 확인 그에 대응하는 숫자를 result에 추가
     public String solution(String rsp) {
-        StringBuilder result = new StringBuilder();  // 결과 문자열을 저장하기 위한 StringBuilder 객체
-
+        String result = "";
         for (int i = 0; i < rsp.length(); i++) {
-            char c = rsp.charAt(i);  // 문자열 rsp에서 i 해당위치에 문자를 반환하는 메서드
-
+            char c = rsp.charAt(i);
             if (c == '2') {
-                result.append('0');  // 가위(2)는 바위(0)를 이김
+                result += '0';
             } else if (c == '0') {
-                result.append('5');  // 바위(0)는 보(5)를 이김
+                result += '5';
             } else if (c == '5') {
-                result.append('2');  // 보(5)는 가위(2)를 이김
+                result += '2';
             }
         }
-
-        return result.toString();
+        return result;
     }
 
     public static void main(String[] args) {
