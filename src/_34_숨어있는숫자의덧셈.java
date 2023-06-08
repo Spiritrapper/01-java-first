01db6bdebb3d22c3933000bf031a59277747e530
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-08 15:02:16 +0900

    문제 사안
    3: Solution34 클래스 선언
    5: solution 메서드 선언. 문자열 my_string을 입력받아 숨겨진 숫자들의 합을 계산하여 반환하는 메서드.
    6: 숫자의 합을 저장할 변수 sum 초기화.
    7: 비교에 사용할 변수 n 초기화.
    8: 문자열 my_string을 한 글자씩 나누어 배열 words에 저장.
    10: 배열 words의 길이만큼 반복.
    11: 현재 원소가 n과 같지 않으면 0을 반환하고 종료.
    13: 원소가 n과 같으면 해당 원소를 반환하고 종료.
    14: 원소가 n과 같을 경우에도 이후 코드는 실행되지 않으므로 sum이 업데이트되지 않음.
    17: 반복문이 종료된 후에는 sum을 반환.
    20: main 메서드 선언.
    22: Solution34 객체 생성.
    25-27: 첫 번째 예시 입력에 대해 solution 메서드 호출하여 결과 출력.
    30-32: 두 번째 예시 입력에 대해 solution 메서드 호출하여 결과 출력.
    코드를 분석해보면, 주어진 문자열에서 숨겨진 숫자들의 합을 계산하는 것이 목표입니다. 하지만 코드의 로직에 문제가 있습니다. 몇 가지 문제점을 지적하겠습니다:
    
    수정 사안
    
    7번 줄에서 n을 초기화했지만, 이후에 n의 값이 변경되지 않고 사용되지 않습니다. 이 부분은 제거해도 됩니다.
    11번 줄에서 배열의 원소와 n을 비교할 때, != 연산자를 사용했습니다. 그러나 문자열 비교에는 != 연산자를 사용해서는 안 됩니다. 문자열을 비교할 때는 equals 메서드를 사용해야 합니다.
    13번 줄에서 원소를 반환하고 종료하고자 하지만, 현재 메서드의 반환 타입은 int입니다. 따라서 문자열을 반환할 수 없습니다. 이 부분을 수정해야 합니다.
    14번 줄에서는 sum += words[i]; 문장이 실행되지 않습니다. 이유는 앞서 13번 줄에서 메서드가 종료되었기 때문입니다. 따라서 13번 줄과 14번 줄의 위치가 바뀌어야 합니다.
    17번 줄에서는 반복문이 종료된 후에 sum을 반환하는 것이 올바릅니다. 하지만 현재는 return 문이 반복문 안에 있으므로 반복문이 한 번 실행되고 종료됩니다. 이 부분을 수정해야 합니다.

--- src/_34_숨어있는숫자의덧셈.java
+++ src/_34_숨어있는숫자의덧셈.java
@@ -1,25 +1,16 @@
 class Solution34 {
-
-    public int solution(String my_string) {
-        int sum = 0;  // 숫자의 합을 저장할 변수
-        StringBuilder numStr = new StringBuilder();  // 숫자를 임시로 저장할 StringBuilder 객체
-
-        for (char c : my_string.toCharArray()) {  // 문자열을 한 글자씩 순회
-            if (Character.isDigit(c)) {  // 현재 문자가 숫자인지 확인
-                numStr.append(c);  // 숫자인 경우 numStr에 추가
-            } else {
-                if (numStr.length() > 0) {  // 숫자가 저장된 경우
-                    sum += Integer.parseInt(numStr.toString());  // numStr에 저장된 숫자를 합산
-                    numStr.setLength(0);  // numStr 초기화
-                }
-            }
+    public int solution(String my_string){
+        int sum = 0;
+        int n =0;
+        String[] words = my_string.split("");
+        for (int i = 0; i < words.length; i++) {
+            if(words[i] != n ){
+                return 0;
+            }else
+                return words[i];
+            sum += words[i];
         }
-
-        if (numStr.length() > 0) {  // 숫자가 남아있는 경우
-            sum += Integer.parseInt(numStr.toString());  // numStr에 저장된 숫자를 합산
-        }
-
-        return sum;  // 결과 합 반환
+        return sum;
     }
 
     public static void main(String[] args) {
@@ -32,6 +23,7 @@ class Solution34 {
         String my_string2 = "1a2b3c4d123";
         int result2 = solution.solution(my_string2);
         System.out.println(result2); // 16
+
     }
 }
 
