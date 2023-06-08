d1cd8a100d04735d8d29aa38978fa72131760956
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-08 15:57:11 +0900

    문제 사안
    solution 메서드는 String 값을 반환해야하므로 반환 타입을 int에서 String으로 변경합니다.
    str1(i)는 올바른 문법이 아닙니다. 올바른 방법은 str1.charAt(i)를 사용하여 str1의 i번째 문자를 가져오는 것입니다.
    return " "은 빈 문자열을 반환하는 것이 아니라 공백 문자열을 반환하는 것입니다. 빈 문자열을 반환하려면 return ""을 사용해야 합니다.
    answer 변수가 선언되어 있지 않아 해당 부분을 수정하지 않고 주석 처리했습니다.
    마지막으로, for 루프 내부의 if 문이 현재 상황에서 항상 true가 되므로, return 문이 항상 실행되어 for 루프가 한 번만 실행될 것입니다. 이로 인해 첫 번째 문자만 처리하고 나머지 문자들은 처리되지 않을 수 있습니다. 이 부분을 원하는 동작에 맞게 수정해야 합니다.

---
+++ src/_35_문자열안에문자열.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Solution35 {
+    public int solution(String str1, String str2) {
+        for (int i = 0; i < str1.length(); i++) {
+            if(i<2 && i<6){
+                return str1(i);
+                sum += str1.charAt(i);
+            }else
+                return "";
+        }
+
+        return sum;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class _35_문자열안에문자열 {
+}
