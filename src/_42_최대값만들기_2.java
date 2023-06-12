ba4b7fbb96e1e8ced16ec66fcc94f13facbdee23
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-12 19:20:36 +0900

    수정 사안
    max = Integer.MIN_VALUE는 자바에서 제공하는 Integer 클래스의 MIN_VALUE 상수입니다. 이 값은 int 데이터 타입의 최솟값을 나타내는 상수
    
    if (product > max)는 현재까지의 최댓값 max보다 현재의 곱셈 결과 product가 더 큰 경우에만 max 값을 업데이트하는 조건문입니다. 이렇게 함으로써 반복문을 통해 모든 가능한 곱셈 결과를 비교하면서 최댓값을 갱신할 수 있습니다.
    
    만약 if (product >= max)와 같이 등호를 포함하여 비교한다면, 같은 값이 나왔을 때에도 max를 업데이트하게 됩니다. 이 경우에는 최댓값을 찾는 목적에 맞지 않으므로, >를 사용하여 업데이트를 수행합니다.
    
    따라서, if (product > max)를 사용하여 현재 곱셈 결과가 최댓값보다 큰 경우에만 최댓값을 업데이트하는 것이 맞는 방식

--- src/_42_최대값만들기_2.java
+++ src/_42_최대값만들기_2.java
@@ -1,15 +1,40 @@
-class Solution {
+class Solution42 {
     public int solution(int[] numbers) {
+        int max = Integer.MIN_VALUE; // 최솟값으로 초기화
+        
         for (int i = 0; i < numbers.length; i++) {
             for (int k = 0; k < numbers.length; k++) {
-                 int max = numbers[i]*numbers[k]
+                if (numbers[i] == numbers[k]) {
+                    continue;
+                }
+                int product = numbers[i] * numbers[k];
+                if( product > max){
+                    max = product;
+                }
 
-            }   
+            }
+               
         }
-        int answer = max ;
+        
         return max;
     }
+    public static void main(String[] args) {
+    Solution42 s = new Solution42();
+    
+    int[] numbers1 = {1, 2, -3, 4, -5};
+    int result1 = s.solution(numbers1);
+    System.out.println(result1); // 출력: 15
+    
+    int[] numbers2 = {0, -31, 24, 10, 1, 9};
+    int result2 = s.solution(numbers2);
+    System.out.println(result2); // 출력: 240
+    
+    int[] numbers3 = {10, 20, 30, 5, 5, 20, 5};
+    int result3 = s.solution(numbers3);
+    System.out.println(result3); // 출력: 600
 }
+}
+
 
 
 
