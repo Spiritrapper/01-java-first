80eff8ffe34f3c29e26826da55477ff4c9b34f69
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-12 19:00:22 +0900

    문제 사안
    max 변수의 선언과 초기화가 반복문 내부에 있습니다. 이 경우 max 변수는 반복문이 실행될 때마다 재선언되고 초기화되므로 의도한 대로 동작하지 않습니다. 또한, max 변수는 두 수의 곱 중 최댓값을 저장해야 하므로 반복문이 실행될 때마다 갱신되어야 합니다.
    
    answer 변수가 max 변수 대신에 반환되고 있습니다. 따라서 answer 변수에 max 변수의 값을 대입해야 합니다.

---
+++ src/_42_최대값만들기_2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int k = 0; k < numbers.length; k++) {
+                 int max = numbers[i]*numbers[k]
+
+            }   
+        }
+        int answer = max ;
+        return max;
+    }
+}
+
+
+
+
+
+
+
+public class _42_최대값만들기_2 {
+    
+}
