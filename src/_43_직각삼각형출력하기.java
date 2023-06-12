3f153e31ac3f9423f87151f36899e5e138aac96d
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-12 22:00:41 +0900

    .

---
+++ src/_43_직각삼각형출력하기.java
@@ -0,0 +1,19 @@
+class Solution43 {
+   public static void main(String[] args) {
+        int n = 3;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print( "*");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+
+
+
+public class _43_직각삼각형출력하기 {
+    
+}
