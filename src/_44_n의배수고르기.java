c1440de2ae4e30eb4124ea4db9a8e3c385c833f0
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-12 22:16:49 +0900

    문제 사안
    int [] numMulti = new numMulti();에서 new numMulti()는 올바른 배열 객체 생성 방법이 아닙니다. 대신 int[] numMulti = new int[count];와 같이 배열의 크기를 지정하여 객체를 생성해야 합니다.
    numlist % n은 올바른 구문이 아닙니다. numlist[i] % n으로 수정해야 합니다.
    numMulti(i) = numlist(i);는 배열의 원소를 할당하는 방법이 잘못되었습니다. 올바른 방법은 numMulti[index] = numlist[i];입니다.
    int[] answer = numMulti[i];는 배열을 반환할 때 올바른 방법이 아닙니다. 올바른 반환 구문은 return numMulti;입니다.

---
+++ src/_44_n의배수고르기.java
@@ -0,0 +1,27 @@
+class Solution44 {
+    public int[] solution(int n, int[] numlist) {
+    int [] numMulti = new numMulti();
+    for(i = 0; i < numlist.length; i++ )
+    {
+        if (numlist % n == 0){
+        numMulti(i) = numlist(i);
+        }
+
+    }
+        int[] answer = numMulti[i];
+        return answer;
+    }
+}
+
+
+
+
+
+public class _44_n의배수고르기 {
+    
+}
+
+
+
+
+
