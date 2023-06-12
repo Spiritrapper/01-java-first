0671ad58e7f00c2c76b28725b162d4acfecb7f62
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-13 08:06:23 +0900

    문제 사안
    int n = 0;을 int n = 3;으로 변경하여 변수 n에 값을 할당합니다.
    count = 0;을 int count = 0;으로 변경하여 count 변수를 선언합니다.
    n을 numlist의 원소와 비교할 때, numlist[i] % n == 0으로 변경하여 n으로 나누어 떨어지는 숫자를 체크합니다.
    n과 numlist의 원소를 비교할 때, solution 메서드의 매개변수 n을 사용하도록 수정합니다.
    결과 출력 시, result1 배열의 원소를 공백을 포함하여 출력합니다.

--- src/_44_n의배수고르기.java
+++ src/_44_n의배수고르기.java
@@ -1,15 +1,32 @@
 class Solution44 {
     public int[] solution(int n, int[] numlist) {
-    int [] numMulti = new numMulti();
-    for(i = 0; i < numlist.length; i++ )
-    {
-        if (numlist % n == 0){
-        numMulti(i) = numlist(i);
+        int n = 0;
+        count = 0; // n으로 나누어 떨어지는 숫자의 개수를 저장하기 위한 변수
+        for (int i = 0; i < numlist.length; i++) {
+            if (numlist[i] % n == 0) {
+                count++;
+            }
         }
-
+        
+        int[] numMulti = new int[count]; // count 값을 가지고 새로운 배열을 생성
+        
+        int index = 0;
+        for (int i = 0; i < numlist.length; i++) {
+            if (numlist[i] % n == 0) {
+                numMulti[index] = numlist[i]; // n으로 나누어 떨어지는 숫자를 새로운 배열에 저장
+                index++;
+            }
+        }
+        
+        return numMulti;
     }
-        int[] answer = numMulti[i];
-        return answer;
+    public static void main(String[] args) {
+        Solution44 s = new Solution44();
+        n = 3;
+        int [] numlist1 = {4, 5, 6, 7, 8, 9, 10, 11, 12};
+        int [] result1 = s.solution(3, numlist1);
+        System.out.println(result1);
+        
     }
 }
 
