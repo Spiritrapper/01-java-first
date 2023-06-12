f764248b22e7fbe2bedfd71b5762974149ebe3bc
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-13 08:50:20 +0900

    문제 사안
    String [] words = new my_string.split(); 줄에서 my_string을 String 객체로 생성하고 split() 메서드를 호출해야합니다. new 키워드는 필요하지 않습니다. 올바른 코드는 다음과 같습니다: String[] words = my_string.split(" ");
    
    배열의 인덱스 접근 시에는 []를 사용해야하지만, words(i)와 같이 괄호를 사용하고 있습니다. 올바른 코드는 words[i]입니다.
    
    int [] int1 = new int1[count]; 줄에서 배열을 생성할 때 new 키워드와 타입 이름을 함께 사용해야합니다. 따라서 올바른 코드는 int[] int1 = new int[count];입니다.
    
    int1[i] = words[i]; 줄에서 words[i]는 String 형식의 원소이고, int1 배열은 int 형식으로 선언되어 있습니다. 서로 다른 형식이므로 이를 일치시켜주어야 합니다. 예를 들어, int1[i] = Integer.parseInt(words[i]);와 같이 String을 int로 변환하여 대입할 수 있습니다.
    
    index 변수가 선언되지 않았습니다. 이를 추가해주어야 합니다. 예를 들어, int index = 0;과 같이 선언할 수 있습니다.

---
+++ src/_45_문자열정렬하기_1.java
@@ -0,0 +1,33 @@
+class Solution45 {
+    public int[] solution(String my_string) {
+        int n =0;
+        int count = 0;
+        String [] words = new my_string.split();
+        for (int i = 0; i < words.length; i++) {
+            if(words(i) == n ){
+                count++;
+            }
+            
+        }
+        int [] int1 = new int1[count];
+        for (int i = 0; i < int1.length; i++) {
+            if(words(i) == n){
+                int1[i] = words[i];
+                index++;
+            }
+            
+        }
+        return int1;
+    }
+    public static void main(String[] args) {
+        Solution45 s = new Solution45();
+        String my_string1 = "hi12392";
+        int [] result1 = s.solution(my_string1)	
+	
+    }
+}
+
+
+public class _45_문자열정렬하기_1 {
+    
+}
